125be009dae7d00f8424f83988ff4b88d94cefbc
Jeffrey Ma <jjeffreyma@gmail.com>
2017-03-01 09:57:57 -0800

    Hack-hour-1 FizzBuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,25 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    if (isNaN(num) || num === null || num < 0) {
+        return 'Input is invalid!';
+    }
+    if (num === 0) {
+        return ['fizzbuzz'];
+    }
+    const result = [];
+    for (let i = 1; i <= num; i += 1) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push('fizzbuzz');
+        } else if (i % 3 === 0) {
+            result.push('fizz');
+        } else if (i % 5 === 0) {
+            result.push('buzz');
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
 }
 
 module.exports = fizzbuzz;

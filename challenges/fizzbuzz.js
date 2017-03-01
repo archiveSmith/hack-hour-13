4b54930d19a2fc4a77c220d31288cf6a06556479
Johnny Coyle <johnnycoyle@gmail.com>
2017-03-01 09:53:41 -0800

    hack-hour 1

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,18 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+    let arr = [];
 
+    if(!Number.isInteger(num)) return undefined;
+
+    for(let i = 1; i <= num; i++){
+        if (i%3 === 0 || i%5 ===0){
+            if (i%3 === 0 && i%5 === 0) arr.push('fizzbuzz');
+            else if (i%3 === 0) arr.push('fizz');
+            else if (i%5 === 0) arr.push('buzz');
+        } else arr.push(i);
+    }
+    return arr;
 }
 
 module.exports = fizzbuzz;

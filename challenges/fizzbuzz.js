cbbfb861b67bdf71d48fb687451b95c37944a88f
Masaya Kawauchi <mkawauchi@Masayuh.local>
2017-03-01 09:52:13 -0800

    finished fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,22 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  //for loop from 1 - num
+  let result = [];
+  for(let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('fizzbuzz');
+    }
+    else if (i % 3 === 0) {
+      result.push('fizz');
+    }
+    else if (i % 5 === 0) {
+      result.push('buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = fizzbuzz;

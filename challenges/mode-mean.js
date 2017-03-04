7a567be404beec34b686807bd2dfc2d619eacb7d
Justin Pierson <justinpierson@Justins-MacBook-Pro.local>
2017-03-04 09:53:20 -0800

    modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
-
+    let sum = 0;
+    let mean;
+    let mode;
+    let modeObj = {};
+    
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    mean = Math.floor(sum/array.length);
+    
+    
+    for (let i = 0; i < array.length; i++) {
+      if (!modeObj.hasOwnProperty(array[i])) {
+        modeObj[array[i]] = 1;
+      } else {
+        modeObj[array[i]] = modeObj[array[i]] += 1;
+      }
+    }
+    mode = Number(Object.keys(modeObj).reduce(function(a, b) {
+      if (modeObj[a] > modeObj[b]) {
+        return a;
+      } else {
+        return b;
+      }
+    }));
+    return mode === mean;
 }
 
 module.exports = modemean;

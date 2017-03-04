d2cc9278d416e6f5c889a1e6d865ddf926ef86f4
Glenn Alexander <glennalexander@Glenn-Alexanders-MacBook-Pro.local>
2017-03-04 09:55:04 -0800

    Completed Modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,34 @@
 
 
 function modemean(array) {
+    if(!array.length) {
+        console.log("No entries provided");
+        return;
+    }
 
+    let mean = 0;
+    let mode;
+    let max = 0;
+    let modes = [];
+    const storage = {};
+
+    array.forEach(function(x) {
+        mean += x;
+        if (storage[x] === undefined) {
+            storage[x] = 1;
+        } else storage[x]++;
+    })
+
+    for (key in storage) {
+        if (storage[key] >= max) {
+            max = storage[key];
+            modes.push(key);
+        }
+    }
+
+    mean = Math.floor(mean / array.length);
+    mode = Math.max.apply(null, modes);
+    return mean === mode;
 }
 
 module.exports = modemean;

25da71eddb1174fb3e98561f1c5f7b091e88defc
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-03-04 10:15:50 -0800

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,36 @@
 
 
 function modemean(array) {
+    //find mode
+    runningMode = {};
+    let mode;
+    let temp;
+    let mean = 0;
+    for (i=0; i<array.length; i++) {
+        mean += array[i]; 
+        if (runningMode[array[i]] === undefined) {
+            runningMode[array[i]] = 1;
+        } else {
+            runningMode[array[i]]++;
+        }
+    }
+    mean = Math.floor(mean/array.length);
 
+    for (key in runningMode) {
+        if (mode !== undefined) {
+            if (runningMode[key] > runningMode[mode]) {
+                mode = key;
+            } else if (runningMode[key] === runningMode[mode]) {
+                mode = parseInt(key) > parseInt(mode) ? key : mode
+            }
+        } else {
+            mode = key;
+        }
+    }
+    console.log(runningMode)
+    return parseInt(mode) === mean;
 }
+
+//console.log(modemean([3,5,7,5,6,8]));
 
 module.exports = modemean;

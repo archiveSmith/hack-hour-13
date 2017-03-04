b25b783310500ecea5a881628ab26a61b94978c0
Masaya Kawauchi <mkawauchi@Masayuh.local>
2017-03-04 09:17:24 -0800

    finished challenge

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -14,7 +14,18 @@
 */
 
 function drawStairs(n) {
-
+  let hash;
+  for(var i = 0; i < n; i++) {
+    hash = "";
+    for(var j = 0; j < n; j++) {
+      if(n - i - 2 < j) {
+        hash += "#";
+      } else {
+        hash += " ";
+      }
+    }
+  console.log(hash);
+  }
 }
 
 

1e6b8043f598a69112ac09ea0ea992d909700265
Titan0723 <yu.angela.c@gmail.com>
2017-03-04 09:49:20 -0800

    angelas hack hour 3

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,35 @@
 
 
 function modemean(array) {
+    var sum = 0;
+    for(var i = 0 ; i < array.length ; i++){
+        sum++
+    }
+    var average = sum/array.length;
 
+    var isMode = {};
+    for(var i = 0; i < array.length ; i++){
+        if (array[i] in isMode) {
+            isMode[array[i]] += 1;
+        }
+        else {
+            isMode[array[i]] = 0;
+        }
+    }
+    // console.log(average);
+    // console.log(isMode);
+    var mode = Object.keys(isMode).reduce(function(a, b){ return isMode[a] > isMode[b] ? a : b });
+    // console.log(mode);
+    // console.log("debug 2" +mode + "_"+average);
+    if(average === mode) {
+        return true;
+    }else{
+        return false;
+    }
 }
+
+var arr1 = [1,1,2,2,2,3,4,5];
+var arr2 = [1,4,5,6,7];
+console.log(modemean(arr1));
 
 module.exports = modemean;

8ab46c764c519fbe8cde9ffa6144a5220f28229c
Kangseon Cho <kangseon.cho@Kangseons-MacBook-Air.local>
2017-03-05 16:58:35 -0800

    finished hackhour 13 modeMean properly

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,48 +9,19 @@
  *
  */
 
+ function modemean(array, memo = {}) {
+   const mean = Math.floor((array.reduce((accum, curr) => accum + curr)) / array.length);
 
- let arr = [1,2,1,2,3]
-
- function modemean(array) {
-   let mean = Math.floor( (array.reduce((accum,curr) => accum+curr)) / array.length );
-
-   let modeCount = 1;
-   let contestCount = 0;
-   let mode = array[0];
-   let contestant;
-
-   let sorted = array.sort(function(a,b){return a-b})
-   console.log(sorted)
-
-     for (let i=0; i < sorted.length; i++) {
-       console.log("mode is",mode)
-       console.log("contestant is ",contestant)
-       console.log("prev counter is ", modeCount)
-       console.log("next counter is ", contestCount + "\n")
-       if (sorted[i] === sorted[i+1] && modeCount > contestCount) {
-         modeCount++;
-         mode = sorted[i];
-         contestant = sorted[i+1];
-       } else if (sorted[i] === sorted[i+1] && modeCount < contestCount) {
-         contestCount++;
-         mode = sorted[i];
-         contestant = sorted[i+1];
-       } else if (sorted[i] !== sorted[i+1] && modeCount > contestCount) {
-         contestCount++;
-         mode = sorted[i];
-         contestant = sorted[i+1]
-       } else if (sorted[i] !== sorted[i+1] && modeCount <= contestCount) {
-         contestCount = 0;
-         modeCount = 0;
-         mode = sorted[i+1];
-         contestant = undefined;
-       }
+   for (let i = 0; i < array.length; i++) {
+     if(memo.hasOwnProperty(memo[array[i]])) {
+       memo[array[i]]++;
+     } else {
+     memo[array[i]] = 1;
      }
+   }
 
-   return mode;
+   const mode = Number(Object.keys(memo).reduce((accum, curr) => memo[accum] > memo[curr] ? accum : curr));
+   return mode === mean;
  }
-
- modemean(arr)
 
 module.exports = modemean;

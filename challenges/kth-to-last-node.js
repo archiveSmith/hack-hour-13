f4b2c101906a341d2d5aa1be3ed89695a5ec79ae
Johnny Coyle <johnnycoyle@gmail.com>
2017-03-07 09:56:05 -0800

    completed kth-to-last-node.js hack hour

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -17,12 +17,41 @@
  */
 
 function Node(val) {
-  this.value = val;
-  this.next = null;
+   this.value = val;
+   this.next = null;
 }
 
 function kthToLastNode(k, head) {
+  //get length of linked list
+  let listLength = 0;
+  let currentNode = head;
+  while (currentNode.next){
+    listLength++;
+    currentNode = currentNode.next;
+  }
+  listLength++;
+  currentNode = head;
+  //if k is great than the length of our list, return invalid input error;
+  if (k > listLength) return 'Not a valid input. k > length of LinkedList';
 
+  //loop through list for length - k
+  for (let i = 0; i < listLength - k; i++){
+    currentNode = currentNode.next;
+  }
+  return currentNode.value;
 }
+//
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+//
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+//
+// console.log(kthToLastNode(6, a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};

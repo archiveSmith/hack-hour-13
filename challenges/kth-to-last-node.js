81f14a7646b23068d0f2454e6e6c19be4a34f48c
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-08 09:16:39 -0800

    kth

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Write a function that takes two parameters, an integer and the head of a
  * singly linked list, and returns the VALUE on the kth to last node in the list.
@@ -70,3 +71,34 @@ function kthToLastNode(k, head) {
 
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+||||||| merged common ancestors
+=======
+/**
+ * Write a function that takes two parameters, an integer and the head of a
+ * singly linked list, and returns the VALUE on the kth to last node in the list.
+ *
+ * const a = new Node('A');
+ * const b = new Node('B');
+ * const c = new Node('C');
+ * const d = new Node('D');
+ * const e = new Node('E');
+ *
+ * a.next = b;
+ * b.next = c;
+ * c.next = d;
+ * d.next = e;
+ *
+ * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
+ */
+
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
+
+function kthToLastNode(k, head) {
+
+}
+
+module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+>>>>>>> 28fc9225fc5dbce349e63f4331ec0a1ebcf7ba5a

afec0556e5ef7140c8a0cb4a13367f3a51174653
John C <John@Johns-MacBook-Air.local>
2017-03-08 09:31:29 -0800

    now calculates negative powers

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,11 +2,12 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-  if(!power) return 1;
-  if(power === 1) return power;
+ function pow(base, power) {
+   if(power < 0) return 1 / (base * pow(base, Math.abs(power) - 1));
+   if(power === 0) return 1;
+   if(power === 1) return base;
 
-  return base * pow(base, power-1);
-}
+   return base * pow(base, power-1);
+ }
 
 module.exports = pow;

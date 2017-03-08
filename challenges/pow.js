d288c4a7a25e568e9f333554461a667e4b38d357
Kangseon Cho <kangseon.cho@Kangseons-MacBook-Air.local>
2017-03-08 09:44:03 -0800

    finished exponent maker with neg and pos numbers

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,21 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+ function pow(base, power) {
+   // set some exponent rules:
+   if(power === 1) return base;
+   if(power === 0) return 1;
 
-}
+   //for positive power
+   if(power > 0) {
+     return pow(base,power-1) * base;
+   }
+   //for negative power
+   else if (power < 0) {
+     power = Math.abs(power);
+     let toInverse = pow(base,power-1) * (base);
+     return 1/toInverse;
+   }
+ }
 
 module.exports = pow;

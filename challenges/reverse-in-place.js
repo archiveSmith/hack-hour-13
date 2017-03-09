fbe7ba1c6c4a46e38f36c7b74464fb293e1b8f4c
slhstudio <slhstudio@gmail.com>
2017-03-09 13:55:19 -0800

    done

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -15,6 +15,12 @@
 
 function reverseInPlace(array) {
 
+   for (let i = 0; i < array.length/2; i += 1) {
+          let temp = array[i];
+          array[i] = array[(array.length - 1) - i];
+          array[(array.length - 1) - i] = temp;
+   }
+   return array;
 }
 
 module.exports = reverseInPlace;

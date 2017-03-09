fb94e1c4bbde4507eeded0b3baf3c6944bd18dfb
slhstudio <slhstudio@gmail.com>
2017-03-08 23:06:27 -0800

    done

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+  
+  //handles negative exponents as though they were positive
+	let abs = false;
+	if (power < 0) {
+	  power = Math.abs(power);
+	  abs = true;
+	}
+	
+	let answer = 1;
+	for(i = 0; i < power; i++) {
+	  answer = base * answer;
+	}
+	
+	//make negative again
+	if (abs === true) {
+	  answer =  1 / answer;
+	}
+	return answer;
 }
 
 module.exports = pow;

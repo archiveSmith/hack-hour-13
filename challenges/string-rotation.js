b4526e5a20d4a4ebf41ff8c806cf205bccf36924
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-10 09:59:09 -0800

    second solution accounting for duplicate letters in strings

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -15,14 +15,44 @@ function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
+// function stringRotation(s1, s2) {
+//   if (typeof s1 !== 'string' || typeof s2 !== 'string') {return 'error'}
+//   if (s1.length === 0 || s2.length === 0) {return true}
+
+//   let firstLetter = s1.charAt(0);
+//   let foundIndex = s2.indexOf(firstLetter); 
+//   let testString = s2.slice(foundIndex) + s2.slice(0, foundIndex);
+//   return testString === s1
+// }
+
 function stringRotation(s1, s2) {
   if (typeof s1 !== 'string' || typeof s2 !== 'string') {return 'error'}
   if (s1.length === 0 || s2.length === 0) {return true}
 
-  let firstLetter = s1.charAt(0);
-  let foundIndex = s2.indexOf(firstLetter); 
-  let testString = s2.slice(foundIndex) + s2.slice(0, foundIndex);
-  return testString === s1
+  var arr1 = [];
+  var arr2 = [];
+
+  for (let i = 0; i < s1.length; i++) {
+    if (i === 0) {arr1.push(s1)}
+    else {
+      arr1.push(s1.slice(i) + s1.slice(0, i))
+    }
+  }
+
+  for (let i = 0; i < s2.length; i++) {
+    if (i === 0) {arr2.push(s2)}
+    else {
+      arr2.push(s2.slice(i) + s2.slice(0, i))
+    }
+  }
+  
+  var flag = false;
+
+  arr1.forEach(x => {
+    if (arr2.indexOf(x) >= 0 ) {flag = true}
+  })
+
+  return flag; 
 }
 
 /*
@@ -32,6 +62,9 @@ RETURN BOOLEAN
   idea 2: find index of first letter of first word in second string and then make string from that letter on   
   slice(foundIndex) + slice(0, foundIndex)
 
+  idea3: loop thru each words making iterations of them starting on different chars, if any one of the iterations for both strings are the same: true; 
+  foundIndex = i and store strings in an array;
+  loop thru one array using indexOf to see if one value is in other array 
 
 */ 
 

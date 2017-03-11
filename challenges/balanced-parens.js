1e857d1afe624f44b0c299103b1f7642b9666f10
Jordan Bautista <jordanbau@gmail.com>
2017-03-11 10:04:58 -0800

    balanced Parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,40 @@
  */
 
 function balancedParens(input){
+    let stripped = input.replace(/[^()[]{}]/g,'');
+    let openers = ['[','(','{']
+    //base cases
+    if(stripped.length % 2 != 0) return false;
+    if(stripped.length === 0) return true;
 
+    function checkParens(input){
+        if(input.length %2 != 0) return false
+        if(input.length === 2) return true;
+    }
 }
 
-module.exports = balancedParens;
+
+
+
+
+
+
+function balancedParens(input){
+    let stripped = input.replace(/[^()[]{}]/g,'');
+    let openers = ['[','(','{']
+    let closers =  [']',')','}']
+    let open = 0;
+    let close = 0;
+
+    if(openers.indexOf(stripped[0]) === -1) return false;
+    if(stripped.length % 2 != 0) return false;
+
+    for(let i = 0; i<stripped.length; i+=1){
+        if (openers.indexOf(stripped[i]) >= 0) open += 1;
+        if (closers.indexOf(stripped[i]) >= 0) close += 1;
+    }
+    return open === close;
+}
+
+console.log(balancedParens('(()){}'))
+// module.exports = balancedParens;

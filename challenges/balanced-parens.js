912cea00775bb3b43b4da7637f5ad454722cf78c
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-11 09:39:40 -0800

    balanced parens complete

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,35 @@
  *
  */
 
-function balancedParens(input){
+ function balancedParens2(input){
+   let open = 0;
+   let closed = 0;
+   for (i = 0; i < input.length; i++) {
+     if (input[i] === '(') open += 1;
+     else if (input[i] === ')') closed += 1;
+     if (closed > open) return false;
+   }
+   return open === closed;
+ }
 
-}
+
+ function balancedParens(input){
+   const parensStack = [];
+   let popResult = true;
+   function stackPop(char){
+     if (parensStack[parensStack.length - 1] === char) {
+       return parensStack.pop();
+     } else return '';
+   }
+   for (let i = 0; i < input.length; i += 1) {
+     if (input[i] === '(' || input[i] === '[' || input[i] === '{') {
+       parensStack.push(input[i]);
+     } else if (input[i] === ')') popResult = stackPop('(');
+     else if (input[i] === ']') popResult = stackPop('[');
+     else if (input[i] === '}') popResult = stackPop('{');
+     if (!popResult) return false;
+   }
+   return parensStack.length === 0;
+ }
 
 module.exports = balancedParens;

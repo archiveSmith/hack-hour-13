0acefe82f05838e437cd42025652ebb0c13c7edf
slhstudio <slhstudio@gmail.com>
2017-03-10 20:47:42 -0800

    done

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,9 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+	if (s1.length !== s2.length) return false;
+  	s1 = s1.concat(s1);
+  	return isSubstring(s1, s2);
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};

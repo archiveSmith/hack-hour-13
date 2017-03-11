7938bb6e612e423934d8f49655cd4a92b3d79814
Kevin Qiu <kami.kevinq@gmail.com>
2017-03-11 14:23:08 -0800

    finished blaanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,74 @@
  */
 
 function balancedParens(input){
+  let history = [];
+
+  let string = input.match(/([\(\)\{\}\[\]])/g);
+
+  if (string.length % 2 !== 0) return false;
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === '{') history.push('{');
+    else if (string[i] === ']') history.push(']');
+    else if (string[i] === '(') history.push('(');
+
+    else {
+      if (string[i] === ')' && history[history.length - 1] !== '(') {
+        return false;
+      } else if (string[i] === '}' && history[history.length - 1] !== '{') {
+        return false;
+      } else if (string[i] === ']' && history[history.length - 1] !== '[') {
+        return false;
+      } else history.pop();
+    }
+  }
+
+  return true;
+
+
+
+
+
+  // let brackets = {
+  //   '{' : 0,
+  //   '}' : 0,
+  //   '(' : 0,
+  //   ')' : 0,
+  //   '[' : 0,
+  //   ']' : 0
+  // }
+  //
+  // for (let i = 0; i < input.length; i += 1) {
+  //
+  //   if (brackets['{'] < brackets['}'] || brackets['('] < brackets[')'] || brackets['['] < brackets[']']) return false;
+  //
+  //
+  //
+  //   if (input[i] === '{') brackets['{'] += 1;
+  //   else if (input[i] === '}') brackets['}'] += 1;
+  //   else if (input[i] === '(') brackets['('] += 1;
+  //   else if (input[i] === ')') brackets[')'] += 1;
+  //   else if (input[i] === '[') brackets['['] += 1;
+  //   else if (input[i] === ']') brackets[']'] += 1;
+  // }
+  //
+  //
+  // // console.log(brackets['('], brackets[')']);
+  // // console.log(brackets['{'], brackets['}']);
+  // // console.log(brackets['['], brackets[']']);
+  //
+  // return (brackets['{'] === brackets['}'] && brackets['('] === brackets[')'] && brackets['['] === brackets[']']);
 
 }
+
+
+console.log(balancedParens('('));  // false   balancedParens('()'); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
 
 module.exports = balancedParens;

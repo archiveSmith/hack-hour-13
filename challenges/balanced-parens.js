aa38a9c1acc3594c4f9f9004924b0eb07dc42cef
slhstudio <slhstudio@gmail.com>
2017-03-12 01:20:41 -0800

    done

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,6 +25,31 @@
  */
 
 function balancedParens(input){
+  if (typeof input !== 'string' || input === undefined) {
+    return "Please enter a string!";
+  }
+  //creates string of brackets, each set balanced. 
+  //This will be used to check against for what next bracket should be.
+  let parens = "[]{}()", openBrackIndexPos = [], brackPosition;
+
+  for(let i = 0; input[i]; i++) {
+  	//knowing this index determines if it's a bracket AND if it's an opening bracket or not
+    brackPosition = parens.indexOf(input[i]);
+    
+    //skips over characters that are not brackets
+    if (brackPosition === -1) {
+      continue;
+    }
+    //If parens is opening parens (according to declared parens variable)
+    if (brackPosition % 2 === 0) {
+    //then push its index to array. + 1 is index in parens of next expected bracket.
+      openBrackIndexPos.push(brackPosition + 1); 
+      //
+    } else if (openBrackIndexPos.pop() !== brackPosition) 
+        return false;
+  }
+ 
+  return openBrackIndexPos.length === 0;
 
 }
 

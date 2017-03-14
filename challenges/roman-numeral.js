f4476c4534b392cef386ba7cdc1daee3c5921cd0
Steven Myket <stevenmyket@Stevens-MacBook-Pro-2.local>
2017-03-14 10:00:33 -0700

    Finished

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,6 +18,19 @@
  */
 
 function romanNumeral(n) {
+	const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	const roman = ['M', 'CM','D','CD','C', 'XC', 'L', 'XL', 'X','IX','V','IV','I'];
+	let result = '';
+	
+	if (typeof n === 'number') {
+		for (let i = 0; i <= arabic.length; i++) {
+		    while(n % arabic[i] < n) {
+		      result += roman[i];
+		      n -= arabic[i];
+		    }
+		}
+	}
+  return result;
 
 }
 

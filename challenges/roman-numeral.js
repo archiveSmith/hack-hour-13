fa847d1e301d2a8f32adde60a580f8da91d7e3cb
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-03-14 09:49:00 -0700

    christian completed roman-numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,20 @@
  */
 
 function romanNumeral(n) {
-
+	if(Number.isInteger(n) && n > 0 ){
+		let romanList = 
+		{
+			M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1
+		}
+		let result = '';
+		for(let key in romanList){
+			while(n >=  romanList[key]){
+				result	= result.concat(key);
+				n -= romanList[key];
+			}
+		}
+		return result;
+	}
 }
 
 module.exports = romanNumeral;

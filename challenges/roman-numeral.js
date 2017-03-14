00c4fcdc707314911d0cef081e9cd7c59bb1cf90
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-03-14 10:01:05 -0700

    Completed Roman Numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,31 @@
  */
 
 function romanNumeral(n) {
+    if (isNaN(n) || n <= 0) return 'Invalid input.  Should be a positive integer';
+    let output = '';
+    const nums = [{ val: 1000, char: 'M' },
+    { val: 900, char: 'CM' },
+    { val: 500, char: 'D' },
+    { val: 400, char: 'CD' },
+    { val: 100, char: 'C' },
+    { val: 90, char: 'XC' },
+    { val: 50, char: 'L' },
+    { val: 40, char: 'XL' },
+    { val: 10, char: 'X' },
+    { val: 9, char: 'IX' },
+    { val: 5, char: 'V' },
+    { val: 4, char: 'IV' },
+    { val: 1, char: 'I' }];
 
+    nums.reduce((num, obj) => {
+        let count = Math.floor(num / obj.val);
+        for (let i = 0; i < count; i++) {
+            output += obj.char;
+        }
+        return num - (obj.val * count);
+    }, n);
+    
+    return output;
 }
 
 module.exports = romanNumeral;

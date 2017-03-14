c5da51e3e4a3a79c8cf0acd53f0c14cd2844b656
eveafeline <eveafeline@Naomis-MacBook-Pro.local>
2017-03-14 10:14:36 -0700

    add edge cases and update typo

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -19,7 +19,7 @@
 
 function romanNumeral(n) {
   const romanNum = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M' };
-
+  if (n <= 0 || n >= 4000) return 'Please input value between 1-3999 (inclusive)';
   const strArr = [];
   let i = 1;
   let m = n;
@@ -32,7 +32,7 @@ function romanNumeral(n) {
     } else if (rom === 5) {
       strArr.unshift(romanNum[5 * i]);
     } else if (rom > 5 && rom < 9) {
-      strArr.unshift(romanNum[5 * i]+romanNumber[1 * i].repeat(rom));
+      strArr.unshift(romanNum[5 * i] + romanNum[1 * i].repeat(rom));
     } else if (rom === 9) {
       strArr.unshift(romanNum[9 * i]);
     } else {

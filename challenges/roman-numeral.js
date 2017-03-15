8c0b530bb8ef174dfc3da9151e6531ba76cbb2ad
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-15 09:24:11 -0700

    roman numerals and subsetSum

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,23 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
+ function romanNumeral(n, prev = '') {
+   //throw an error if not a non-negative integer
+   if (!Number.isInteger(n) || n < 0) return new Error('n is not a non-negative integer');
 
-}
+   //basecases
+   if (n === 0) return prev;
+
+   //recursive cases
+   const numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+   const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+   for(let i = 0; i < 13; i += 1) {
+     if (n >= values[i]) return romanNumeral(n - values[i], prev + numerals[i]);
+   }
+ }
 
 module.exports = romanNumeral;

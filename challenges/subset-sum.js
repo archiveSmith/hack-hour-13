7f8a91eff4a9f04db0bb71cb87822930523779dc
eveafeline <eveafeline@Naomis-MacBook-Pro.local>
2017-03-15 10:04:59 -0700

    hack hour challenge revised

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -10,14 +10,15 @@
 
 function subsetSum(array, target) {
   // if array contains nothing, return error msg
-  if (array.length === 0) return 'Please input array';
+  if (array.length === 0) return [];
   if (arguments.length < 2) return 'Please input array and target value';
   if (typeof target !== 'number') return false;
   // sum array
   array.sort((a, b) => b - a);
-  let total = array.reduce((a,b) => a+b);
+  let total = array.reduce((a, b) => a + b);
   // subtract from largest to smallest
-  for (let i = 0 ; i < array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === target) return true;
     total -= array[i];
     if (total < target) {
       total += array[i];

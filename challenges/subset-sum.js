950a8f65813bc2414deaff3c0bb2af885c6279aa
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-15 22:23:52 -0700

    sub

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,34 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
 
+function subsetSum(array, target) {
+    if (target === 0) return true; 
+    if (!array.length) return false; 
+    return subsetSum(array.slice(1), target) || subsetSum(array.slice(1), target - array[0]); 
 }
+
+console.log(subsetSum([3, 7, 4, 2], 5))  //true
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)) //true
+console.log(subsetSum([8, 2, 4, 12], 13)) // false
+console.log(subsetSum([8, -2, 1, -3], 6)) //true
+
+
+/*
+break into single values
+match all values with all possible combinations 
+match 1 value with 1 value (2 value matching)
+match 3 values, etc.  
+
+reduce recursion? 
+create every possible subset array and add those values up in each array; if one returns equal to target then true 
+*/
+
+
+
+
+
+
+
 
 module.exports = subsetSum;

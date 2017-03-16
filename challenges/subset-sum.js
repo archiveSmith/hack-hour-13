0e92355cc8bb79b24f9d1a329d6bd72b64a513af
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-16 09:38:15 -0700

    subset

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,11 +8,45 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
+//brute force method (slow); 
+function subsetSum1(array, target) {
+    const subsets = [[]];  //array[0] === 3  [[], [3]]
+    for (let i = 0; i < array.length; i +=1) {
+        for (let j = 0, len = subsets.length; j < len; j+= 1) {
+            const subset = array[i].concat(subset[j]); 
+            if (subset.reduce((a,b) => a + b) === target) {
+                return true;
+            } 
+            subsets.push(subset); 
+        }
+    }
+    return false; 
+}
 
+
+//decrement target until reaches zero, if it reaches 0, then return true;
+//also array has to become smaller and smaller
+//but doesn't account for negative numbers 
+function subsetSum2(array, target) {
+    for (let i = 0; i < array.length; i +=1) {
+        if (array[i] === target) return true;
+        if (array[i] < target) {
+            const newArr = array.slice(i + 1);
+            const newTarget = target - array[i];
+            if (subsetSum2(newArr, newTarget)) {
+                return true;
+            }
+        }
+    }
+    return false; 
+}
+
+//subsets include number or don't include number; just need to test for 2 cases 
 function subsetSum(array, target) {
     if (target === 0) return true; 
     if (!array.length) return false; 
-    return subsetSum(array.slice(1), target) || subsetSum(array.slice(1), target - array[0]); 
+    return subsetSum(array.slice(1), target - array[0]) 
+        || subsetSum(array.slice(1), target); 
 }
 
 console.log(subsetSum([3, 7, 4, 2], 5))  //true
@@ -21,16 +55,24 @@ console.log(subsetSum([8, 2, 4, 12], 13)) // false
 console.log(subsetSum([8, -2, 1, -3], 6)) //true
 
 
+
 /*
-break into single values
-match all values with all possible combinations 
-match 1 value with 1 value (2 value matching)
-match 3 values, etc.  
+//every level branches out 2 ways b/c each level has an OR 
+[3,7,4,2] 5
 
-reduce recursion? 
-create every possible subset array and add those values up in each array; if one returns equal to target then true 
+[7,4,2], 2                                      ||   [7,4,2] 5
+[4,2] , -5    ||       [4,2]  2
+                  [2] -2  ||  [2], 2
+                [] 0 || []
+
+
+
+
+
+
+
+
 */
-
 
 
 

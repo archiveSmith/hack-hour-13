9b896e07207c0725456c3ed9314eec12b413a0f5
Kevin Qiu <kami.kevinq@gmail.com>
2017-03-17 10:09:11 -0700

    fnished binary to decimal, working on hexadecimal

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,36 @@
  */
 
 function binToDec(binary) {
+  // convert to Binary - Recursion
+  // binary = binary.split('').reverse().join('');
+  // let startPos = -1;
 
+  // function bin2Dec(bin, start) {
+  //   if (!bin.length) return 0;
+  //   return bin[0] * Math.pow(2, start + 1) + bin2Dec(bin.slice(1), start + 1);
+  // }
+
+  // return bin2Dec(binary, startPos);
+
+
+  // convert to Hexadecimal - Recursion // not complete
+  binary = binary.split('').reverse().join('');
+  let startPos = -1;
+
+  function bin2Dec(bin, start) {
+    if (!bin.length) return 0;
+    return bin[0] * Math.pow(2, start + 1) + bin2Dec(bin.slice(1), start + 1);
+  }
+
+  return bin2Dec(binary, startPos);
 }
+
+
+console.log(binToDec('0'));   
+console.log(binToDec('11')); 
+console.log(binToDec('100'));
+console.log(binToDec('101'));
+console.log(binToDec('0101'));
+
 
 module.exports = binToDec;

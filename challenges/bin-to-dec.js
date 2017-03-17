b6f38892f3c8d5cd7e22a885b5692896cbe2b4a0
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-03-17 11:00:04 -0700

    bin-to-dec completed

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -13,8 +13,35 @@
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
-function binToDec(binary) {
 
+function binToDec(binary) {
+  let number = 0;
+  let level = 0;
+  for (let i = 0; i < binary.length; i += 1) {
+    level = Math.pow(2, binary.length - 1 - i);
+    number += (level)*binary[i];
+  }
+  return number;
+}
+
+
+function decToBin(dec) {
+  let level = 0;
+  let number = dec;
+  let bin = '';
+  const recurse = (lev) => {
+    if (number / Math.pow(2, lev) >= 2) {
+      bin = bin.concat(recurse(lev+1)); 
+    } 
+    if (number / Math.pow(2, lev) >= 1) {
+      number = number - Math.pow(2, lev);  // number = 1
+      return bin.concat('1')
+    } else if (number / Math.pow(2, lev) < 1) {
+      return bin.concat('0')
+    }
+    return bin;  
+  }
+  return recurse(level);
 }
 
 module.exports = binToDec;

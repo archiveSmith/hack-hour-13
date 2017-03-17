3b2a275221b9ee9ef991446529b919eb5b5787b0
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-17 09:35:11 -0700

    bin to dec and dec to bin

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,25 @@
  */
 
 function binToDec(binary) {
+  let sum = 0;
+  for (let i = binary.length - 1; i >= 0; i -= 1) {
+    if (binary[i] === '1') sum += Math.pow(2, binary.length - 1 - i);
+  }
+  return sum;
+}
 
+function decToBin(dec) {
+  let digitArr = [];
+  let twoPow = 1;
+  while (dec >= twoPow * 2) twoPow *= 2;
+  while (dec > 0 || !digitArr.length || twoPow >= 1) {
+    if (dec >= twoPow) {
+      digitArr.push(1);
+      dec -= twoPow;
+    } else digitArr.push(0);
+    twoPow /= 2;
+  }
+  return parseInt(digitArr.join(''), 10);
 }
 
 module.exports = binToDec;

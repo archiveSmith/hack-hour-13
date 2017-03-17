ee521e480295ae753b31bd6b0e864071ea86197e
Mary-Snow <snow.mary.r@gmail.com>
2017-03-17 09:19:29 -0700

    updated subset-sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -7,7 +7,7 @@
  * subsetSum([8, 2, 4, 12], 13) -> false
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
-
+//MY SOLUTION
 function subsetSum(array, target) {
     if(typeof target !== 'number'){
         return "please give me a number"
@@ -16,7 +16,7 @@ function subsetSum(array, target) {
         return typeof value === 'number';
     }
     if(array.length < 1){return false}
-    let numArr = array.filter(isNum).sort(function (a, b){
+    let numArr = array.sort(function (a, b){
         return b - a;
     })
    // console.log(newArr)
@@ -42,8 +42,51 @@ function subsetSum(array, target) {
     }
     return false; 
 }
-console.log(subsetSum([3,7,4,2],5)) // --> True
-console.log(subsetSum([3,34,4,12,5,12],32)) // -->true 
+//BRUTE FORCE METHOD
+// function subsetSum(array, target){
+//     const subsets = [[]]; //array[0] === 3....[[]], [3]
+//     for(let i = 0; i < array.length; i += 1){
+//         for(let j = 0, len = subsets.length; j < len; j += 1){
+//             const subset = array[i].concat(subset[j]);
+//             if(subset.reduce((a, b) => a + b) == target{
+//                 return true;
+//             }
+//             subsets.push(subset);
+//         }
+//     }
+//     return false;
+// }
+
+//LINKED LIST FOR LOOP
+//for(let i = head; i; i = i.next)
+//for a linked list, while  i exists keeps setting i to i.next. 
+//
+// RECURSIVE METHOD WON't ACCOUNT FOR NEGATIVE NUMBERS
+// function subsetSum(array, target) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === target) return true;
+//         if (array[i] < target) {
+//             const newArr = array.slice(i + 1);
+//             const newTarget = target - array[i];
+//             if (subsetSum(newArr, newTarget)) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// Best Practice for subsetSum
+// function subsetSum(array, target){
+//     if(!target) return true; //meaning that you decremented to zero exactly
+//     if(!array.length) return false; //empty arry means no sum was equal to the target
+//     return subsetSum(array.slice(1), target - array[0]) 
+//     || subsetSum(array.slice(1), target);
+// }
+
+
+console.log(subsetSum([3, 7, 4, 2], 5)) // --> True
+console.log(subsetSum([-34, 2], -32)) // -->true 
 console.log(subsetSum([8, 2, 4, 12], 13)) //false
 console.log(subsetSum([8, -2, 1, -3], 6)) //true
 

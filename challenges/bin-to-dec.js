ad79e829034f6e5a9c00db07cd2ebd88aca9d14a
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-03-17 10:01:27 -0700

    Completed bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,11 @@
  */
 
 function binToDec(binary) {
-
+    if (typeof binary !== 'string') return 'Input needs to be a string.'
+    for (let i = 0; i < binary.length; i += 1) {
+        if (binary.charAt(i) !== '0' && binary.charAt(i) !== '1') return 'Input needs to be in binary.'
+    }
+    return binary.split('').reverse().reduce((accum, val, idx) => {return accum += val * Math.pow(2, idx)}, 0);    
 }
 
 module.exports = binToDec;

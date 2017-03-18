9ab1a0ffe2ea4ee33da545105a7464edf4d8ac1e
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-18 09:51:58 -0700

    matchword yay

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -3,12 +3,33 @@
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
 function matchWord(str) {
+  const words = [];
+  for (let i = 0; i < str.length; i += 1) {
+    let word = '';
 
+    while (i < str.length - 1 && str[i].toLowerCase() === str[i].toUpperCase()) {
+      i += 1;
+    };
+
+    while (str[i].toLowerCase() !== str[i].toUpperCase()) {
+      word += str[i].toLowerCase();
+      if (i === str.length - 1) break;
+      else i += 1;
+    }
+
+    if (words[words.length - 1] === word) words.pop();
+    else if (words.includes(word)) return false;
+    else if (word) {
+      let reversedWord = word.split('').reverse().join('');
+      words.push(reversedWord);
+    }
+  }
+  return words.length === 0;
 }
 
 module.exports = matchWord;

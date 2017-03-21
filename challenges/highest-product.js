aeba03fc7d8d89a2b50be9a9442aaec0027e3010
Steven Myket <stevenmyket@Stevens-MacBook-Pro-2.local>
2017-03-21 09:57:12 -0700

    Finished

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,18 @@
  */
 
 function highestProduct(array) {
+	if (array.length < 3) {
+    return 0;
+  }
 
+  array = array.sort(function(a, b) {
+    return a - b;
+  });
+  
+  let firstProduct = array[0] * array[1] * array[array.length - 1];
+  let secondProduct = array[array.length - 1] * array[array.length - 2] * array[array.length -3];
+  
+  return Math.max(firstProduct, secondProduct);
 }
 
 

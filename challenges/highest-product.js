6e7e283fa69405d76049f9876a5299e189d17151
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-03-21 14:18:50 -0700

    chritian done with highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,22 @@
  */
 
 function highestProduct(array) {
-
+	// if the array is less then 3 then return empty array or message
+	// then loop with two nested loops and.start max at 0 and then comopare to product
+	if(array.length < 3) return 'You need at least 3 integers';
+	let max = 0;
+	let list =[], current;
+	for(let i = 0; i < array.length-2; i++){
+		for(let next = i+1; next < array.length -1; next++){
+			list.push(array[i], array[next], array[next+1]);
+			current = list.reduce((acum, next) => acum * next);
+			if(current > max){
+				max = current;
+			}
+			list=[];
+		}
+	}
+	return max;
 }
 
 

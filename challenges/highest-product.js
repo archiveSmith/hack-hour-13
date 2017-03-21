a458cda6333efbf344a180ecbe9979369924afad
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-21 09:36:31 -0700

    completed highest prod --- if the highest product does not exist or is less than 0, it returns 0 (the empty product)

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,25 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+ function highestProduct(array) {
+   if (array.length < 3) return 0;
 
-}
+   const [negs, pos] = array.reduce((arr, num) => {
+     num < 0 ? arr[0].push(num) : arr[1].push(num);
+     return arr;
+   },[[],[]]);
+
+   negs.sort();
+   pos.sort((a,b) => b-a);
+
+   const posProd = pos[0] * pos[1] * pos[2];
+   const negProd = negs[0] * negs[1] * pos[0];
+
+   if (isNaN(posProd) && isNaN(negProd)) return 0;
+   else if (isNaN(posProd)) return negProd;
+   else if (isNaN(negProd)) return posProd;
+   else return posProd > negProd ? posProd : negProd;
+ }
 
 
 module.exports = highestProduct;

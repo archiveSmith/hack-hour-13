4175593cac9ce2d70b40d00ed60cd07f35299d45
Darrick Mckirnan <dtmckirnan@Darricks-MacBook-Pro.local>
2017-03-21 11:05:44 -0700

    finished product again

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,41 +2,56 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+// function highestProduct(array) {
+//     if (array.length < 3) {
+//         return 0;
+//     }
+
+//     let maxArr = array.sort(function (a, b) {
+//         return b-a;
+//     });
+    
+//     let maxRes = maxArr[0] * maxArr[1] * maxArr[2];
+//     let negatives = maxArr.filter((item) => {
+//         return item < 0;
+//     })
+//     let tempPos = maxArr[0] * maxArr[1];
+//     let tempNeg;
+
+//     if (negatives.length <= 1) {return maxRes}
+    
+//     if (negatives.length >= 2) {
+//         negatives.sort(function (a, b) {return a - b;})
+//         tempNeg = negatives[0] * negatives[1];
+//         console.log(tempNeg);
+//     }
+//     else if (negatives.length === array.length) {
+//       negatives.sort(function(a, b) {
+//         return a-b;
+//       })
+//       return negatives[0] * negatives[1] * negatives[array.length - 1];
+//     }
+
+//     if (tempNeg > tempPos) {
+//         return tempNeg * maxArr[0];
+//     } else {
+//         return maxRes;
+//     }
+// }
+
+function highestProduct (array) {
     if (array.length < 3) {
-        return 0;
+        return '0';
     }
-    
-    let maxArr = array.sort(function (a, b) {
-        return b-a;
+
+    array = array.sort(function (a, b) {
+        return a - b;
     });
-    
-    let maxRes = maxArr[0] * maxArr[1] * maxArr[2];
-    let negatives = maxArr.filter((item) => {
-        return item < 0;
-    })
-    let tempPos = maxArr[0] * maxArr[1];
-    let tempNeg;
 
-    if (negatives.length <= 1) {return maxRes}
-    
-    if (negatives.length >= 2) {
-        negatives.sort(function (a, b) {return a - b;})
-        tempNeg = negatives[0] * negatives[1];
-        console.log(tempNeg);
-    }
-    else if (negatives.length === array.length) {
-      negatives.sort(function(a, b) {
-        return a-b;
-      })
-      return negatives[0] * negatives[1] * negatives[array.length - 1];
-    }
+    let first = array[0] * array[1] * array[array.length - 1];
+    let second = array[array.length - 2] * array[array.length - 3];
 
-    if (tempNeg > tempPos) {
-        return tempNeg * maxArr[0];
-    } else {
-        return maxRes;
-    }
+    return Math.max(first, second);
 }
 
 

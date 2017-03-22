2717557c156ec2db03179cde25a4d8a819ae505a
Jelena Gruica <jelenagruica@Jelenas-MacBook-Pro.local>
2017-03-21 21:07:25 -0700

    changes for negative numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,11 +4,31 @@
  */
 
 function highestProduct(array) {
-  var sorted = [];
-  sorted = array.sort();
+  // if(array.length < 3){
+  //   return 0;
+  // }
+  //
+  // var sorted = [];
+  // sorted = array.sort();
+  //
+  // var prod = 1;
+  // prod = (sorted[sorted.length-1] * sorted[sorted.length-2] * sorted[sorted.length-3])
+  // return prod;
 
-  var prod = 1;
-  prod = (sorted[sorted.length-1] * sorted[sorted.length-2] * sorted[sorted.length -3])
+  var prod = 0;
+
+  for (var i=0; i < array.length-2; i++) {
+    for (var j=i+1; j < array.length - 1; j++) {
+      for (var k=j+1; k < array.length; k++) {
+        var tempProd = array[i] * array[j] * array[k];
+
+        if (tempProd > prod) {
+          prod = tempProd;
+        }
+      }
+    }
+  }
+
   return prod;
 }
 

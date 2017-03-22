07e0c0f7ec3d6894667d61dd38d35d9c7208b930
Jordan Bautista <jordanbau@gmail.com>
2017-03-22 09:49:55 -0700

    HH - zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,28 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let current = l1;
+  let l2Next = true;
+  let temp1 = l1;
+  let temp2 = l2;
+
+  while (current.next) {
+    if (l2Next) {
+      temp1 = current.next;
+      current.next = temp2;
+      l2Next = false;
+    }else{
+      temp2 = current.next
+      current.next = temp1
+      l2Nexgt = true;
+    }
+    current = current.next;
+  }
+  
+  if(temp1) current.next = temp1;
+  if(temp2) current.next = temp2;
+
+  return l1;
 };
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };

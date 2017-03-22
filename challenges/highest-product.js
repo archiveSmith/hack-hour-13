af5b4e76966730092d42f8f0bfe5fad686ecb1ec
Jordan Bautista <jordanbau@gmail.com>
2017-03-22 08:42:51 -0700

    updated HH - highestProduct

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,7 +4,19 @@
 
 function highestProduct(array) {
 
-    return Array.isArray(array) && array.length > 3 ? array.sort((a, b) => b - a).slice(0, 3).reduce((all, item) => { all *= item; return all; }) : 'Please provide a valid array';
+    if(!Array.isArray(array) || array.length < 3) return 'err';
+    
+    let sortedArr = array.sort((a,b) =>{return a-b})
+    let allNeg = sortedArr.reduce((all,item,index)=>{
+        if(item > 0) all = false;
+        return all;
+    },true)
+    if(allNeg) return sortedArr.slice(sortedArr.length-3).reduce((all,item,index)=>{all *= item; return all});
+
+    return Math.max(sortedArr[0] * sortedArr[1] * sortedArr[sortedArr.length-1], sortedArr[sortedArr.length-1] * sortedArr[sortedArr.length-2] * sortedArr[sortedArr.length-3])
+
 }
+
+console.log(highestProduct([-1,-2,-4]));
 
 module.exports = highestProduct;

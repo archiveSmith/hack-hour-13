288de940a431ed79f54f1381aff8d81adb535dfb
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-03-22 09:59:00 -0700

    completed zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,29 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let templ1 = {};
+  let templ2 = {};
+  let dest = l1;
+  while (l2 != null) {
+    templ1 = dest.next;
+    if (dest.next === null) {
+      dest.next = l2
+      break;
+    }
+    else {
+      dest.next = l2;
+      templ2 = l2.next;
+      l2.next = templ1;
+      l2 = templ2;
+      dest = templ1;
+    }
+  }
+  return l1;
 };
+
+//  let l1t = {value:"1", next: {value:"3", next:{value:"5", next:{value:"6", next:null}}}};
+//  let l2t = {value:"2", next: {value:"4", next:null}};
+
+// console.log(zip(l1t, l2t));
 
 module.exports = {Node: Node, zip: zip};

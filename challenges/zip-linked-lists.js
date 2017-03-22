8a3436ff61e655493cbd56e09f60fb75bce9e16f
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-03-22 09:51:49 -0700

    Completed zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,40 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (l1.value !== undefined) {
+    let output = new Node(l1.value);
+    let currNode = output;
+    let onl1 = false;
+    let l1Node = l1.next;
+    let l2Node = l2;
+  } else if (l2.value !== undefined) {
+    let output = new Node(l2.value);
+    let currNode = output;
+    let onl1 = true;
+    let l1Node = l1;
+    let l2Node = l2.next;
+  } else return "No linked lists to zip";
+
+  while (l1Node !== null && l2Node !== null) {
+    if (l1Node === null) {
+      currNode.next = new Node(l2Node.value);
+      l2Node = l2Node.next;
+    } else if (l2Node === null) {
+      currNode.next = new Node(l1Node.value);
+      l1Node = l1Node.next;      
+    } else if (onl1) {
+      currNode.next = new Node(l1Node.value);
+      l1Node = l1Node.next;
+    } else if (!onl1) {
+      currNode.next = new Node(l2Node.value);
+      l2Node = l2Node.next;     
+    }
+    currNode = currNode.next;
+    onl1 = !onl1;
+  }
+
+  return output;
 };
 
 module.exports = {Node: Node, zip: zip};
+

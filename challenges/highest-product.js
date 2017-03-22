02d314911b2384ac9e2d87e048129b71229cd052
Michael Khouv <Michael@Michaels-MacBook-Pro.local>
2017-03-22 10:46:52 -0700

    Completed Highest Product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,38 +3,9 @@
  */
 
 function highestProduct(array) {
-  let negatives = 0
-  array.forEach(elem => {
-    if (elem < 0) negatives++;
+  let arr = array.sort((a, b) => {
+    return b - a;
   })
-  if (negatives % 2 !== 0 && !negatives) {
-    array.sort((a,b) => {
-        return b - a;
-    })
-    array = array.slice(0,3)
-    return array.reduce((acc, curr) => {
-      return acc * curr;
-    }, 1);
-  } else if (negatives % 2 === 0 && negatives) {
-    let outputArr = [];
-    array.sort((a,b) => {
-        return b - a;
-    })
-    console.log(array)
-    outputArr.push(array[0]);
-    for (let i = 1; i < array.length; i += 1) {
-      if (array[i] > Math.abs(array[array.length - 1])) {
-        outputArr.push(array[i]);
-      } else if (array[i] < Math.abs(array[array.length - 1])) {
-        outputArr.push(array[array.length - 1]);
-        array.pop();
-      }
-    }
-    outputArr = outputArr.slice(0,3)
-    return outputArr.reduce((acc, curr) => {
-      return acc * curr;
-    }, 1);
-  }
+  return Math.max((arr[0] * arr[1] * arr[2]), (arr[0] * arr[arr.length - 1] * arr[arr.length - 2]))
 }
-console.log(highestProduct([-5,-5,1,7,-6,-7,7]))
 module.exports = highestProduct;

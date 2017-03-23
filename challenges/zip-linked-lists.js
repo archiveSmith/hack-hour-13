6377ae1249d2ee8ce0bf7169a95fdab718856fee
Darrick Mckirnan <dtmckirnan@Darricks-MacBook-Pro.local>
2017-03-23 08:57:03 -0700

    finished zip linked lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,42 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
-};
+//Iterative
+function zip (l1, l2) {
+  if (!l1) {
+    return l2;
+  }
+  if (!l2) {
+    return l1;
+  }
+
+  let head = l1, temp = l1, l1 = l1.next;
+  while (l1 & l2) {
+    temp.next = l2;
+    l2 = l2.next;
+    temp = temp.next;
+
+    temp.next = l1;
+    l1 = l1.next;
+    temp = temp.next;
+  }
+  temp.next = l2 ? l2 : l1;
+  return head;
+}
+
+
+//Recursion
+function zip (l1, l2) {
+  if (!l1) {
+    return l2;
+  }
+  if (!l2) {
+    return l1;
+  }
+  l1.next = zip(l2, l1.next);
+  return l1;
+}
+
+
 
 module.exports = {Node: Node, zip: zip};

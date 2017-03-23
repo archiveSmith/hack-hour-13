4b76e10eb8edbcd5385d657746fafef175c95f3a
Steven Meads <sbmeads@gmail.com>
2017-03-23 08:56:25 -0700

    zip-linked-list attempt

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -5,12 +5,32 @@
  * BONUS: Do this in place
  */
 
-function Node(val) {
-  this.value = val;
-  this.next = null;
-}
+ // const link1 = new Node(1);
+ // link1.next = new Node(3);
+ //
+ // const link2 = new Node(2);
+ // link2.next = new Node(4);
 
-function zip(l1, l2) {
-};
+ function Node(val) {
+   this.value = val;
+   this.next = null;
+ }
+
+ function zip (l1, l2) {
+   let zipList;
+   let temp1;
+   let temp2;
+
+   while (l2.next) {
+     temp1 = l1.next;
+     temp2 = l2.next;
+
+     l1.next = l2;
+     l2.next = temp1;
+     temp1.next = temp2;
+   }
+   zipList = l1;
+   return zipList;
+ }
 
 module.exports = {Node: Node, zip: zip};

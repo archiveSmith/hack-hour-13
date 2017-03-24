5dcde34331a8ece39c1456de3e5afea09a1ab52c
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-03-24 11:19:05 -0700

    completed max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,39 @@
 
 function Stack() {
   // body...
+  const stack = {};
+  let length = 0;
+  let max = -Infinity;
+  this.push = (value) => {
+    let key = length
+    if (value > max) max = value;   
+    stack[key] = { value: value, max : max } ;
+    length +=1;
+    console.log(stack);
+    return length;
+  }
+  this.pop = () => {
+    let key = length;
+    let popped = stack[key - 1];
+    delete stack[key-1];
+    length -= 1;
+    //console.log(stack, ' ', length);
+    return popped;
+  }
+  this.getMax = () => {
+    return stack[length-1].max;
+  }
 }
+
+// let st = new Stack()
+// console.log(st.push(45));
+// console.log(st.push(89));
+// console.log(st.push(23));
+// console.log(st.push(834));
+// console.log(st.push(34));
+// st.pop();
+// st.pop();
+// console.log(st.getMax());
+
 
 module.exports = Stack;

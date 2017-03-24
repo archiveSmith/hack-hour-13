3dd86f9c817e3bc1123d80bb1a47e62b1f6b15a1
John C <John@Johns-MacBook-Air.local>
2017-03-24 13:11:58 -0700

    completed max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -5,9 +5,22 @@
  * getMax should return the largest value currently in the stack.
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
-
 function Stack() {
   // body...
+  this.stack = [];
 }
+
+Stack.prototype.push = function (element) {
+  this.stack[this.stack.length] = element;
+  return this.stack.length;
+};
+
+Stack.prototype.pop = function () {
+  return this.stack.slice(-1);
+};
+
+Stack.prototype.getMax = function () {
+  return Math.max(...this.stack);
+};
 
 module.exports = Stack;

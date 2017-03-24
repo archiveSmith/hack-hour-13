329097961955dfaa5ef7c296f5d2aef56eb7ac79
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-03-24 09:57:05 -0700

    christian completed max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,22 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
-}
+var Stack = function(){
+	this.stakLength = Object.keys(this).length;
+	this.push =  function(input){
+		this[this.stakLength] = input;
+		this.stakLength += 1;
+		return stakLength;
+	};
+	this.pop = function(){
+		var lastElement = this[this.stakLength - 1];
+		delete this[this.stakLength - 1];
+		this.stakLength -= 1;
+		return lastElement;
+	};
+	this.getMax = function(){
+		return Math.max.apply(null, this);
+	};
+};
 
 module.exports = Stack;

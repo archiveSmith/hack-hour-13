720e27bca7d05e063d25ff84402cf6af0e0b037e
Kevin Qiu <kami.kevinq@gmail.com>
2017-03-25 09:41:13 -0700

    finished reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,31 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+	if (!head) return false;
+	if (!head.next) return head;
 
+	let prev = new Node(head.value); 
+	head = head.next;
+
+	for (let i = head; i; i = i.next) {
+		let newHead = new Node(i.value);
+		newHead.next = prev;
+		prev = newHead;
+	}
+
+	return prev;
 }
+
+// var a = new Node(5);
+// var b = new Node(6);
+// a.next = b;
+// var c = new Node(7);
+// b.next = c;
+// var d = new Node(8);
+// c.next = d;
+
+// console.log(a);
+
+// reverseLinkedList(a);
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

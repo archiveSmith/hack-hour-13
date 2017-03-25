ed1b9abb1dd9953fd7d1840dfacd36296e517a0f
slhstudio <slhstudio@gmail.com>
2017-03-25 16:10:00 -0700

    done

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,52 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+	//set current node to head
+	let curr = head;
+	//make array to stash values of list as we iterate through
+	let stash = [];
 
+	//iterate through list. Push each value into array.
+	while (curr.next) {
+		stash.push(curr.value);
+		curr = curr.next;
+	}
+	stash.push(curr.value);
+
+	//make new list
+	let newHead = new Node(stash.pop());
+	//keep track of head
+	let origin = newHead;
+	//loop through stashed array and created new linked node for each value in stashed array starting at end.
+	let len = stash.length;
+	for (let i = 0; i < len; i++) {
+		newHead.next = new Node(stash.pop());
+		newHead = newHead.next;
+  }
+	return origin;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+
+//test:
+
+// let test = new Node(1);
+// let start = test;
+
+// for (let i = 2; i < 11; i++) {
+// 	test.next = new Node(i);
+// 	test = test.next;
+// }
+// console.log('Reversing:')
+
+// test = reverseLinkedList(start);
+
+// while (test.next) {
+// 	console.log(test.value);
+// 	test = test.next;
+// }
+
+// console.log (test.value);
+
+
+

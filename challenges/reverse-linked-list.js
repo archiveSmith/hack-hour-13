e918fd7204a0ba9ef95993eb5fdeb6c9e4815bc2
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-25 09:41:04 -0700

    reverse linked list in place

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,32 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  //Check input type for Error handling
+  if (typeof head !== 'object') throw new Error('Invalid input');
+  if (!head.hasOwnProperty('value') || !head.hasOwnProperty('next')) throw new Error('Invalid input');
 
+  let trailing = head;
+  let middle = trailing.next;
+  let lead = middle.next;
+
+  //Case: linked list length 1 or 2
+  if (!middle) return trailing;
+  else if (!lead) {
+    middle.next = trailing;
+    trailing.next = null;
+    return middle;
+  }
+
+  trailing.next = null;
+  while (lead) {
+    middle.next = trailing;
+    trailing = middle;
+    middle = lead;
+    lead = lead.next;
+  }
+  middle.next = trailing;
+  return middle;
 }
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

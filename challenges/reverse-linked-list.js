f9918594ff5c01c3b0b97abbe35e54acbe2580cf
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-03-25 10:01:26 -0700

    reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,23 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+	this.value = value;
+	this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+	let temp = null;
+	let prev = null;
+	let node = head;
+	if (node === null) return null;
+	while (node && node.next !== null && node.next !== node) {
+		temp = node.next; // store node.next in temp
+		node.next = prev; // make node.next equal to prev
+		prev = node; // set new prev to the current node
+		node = temp; // increment curret node (i.e., set it to temp, which is node.next)
+	}
+	node.next = prev;
+	return node;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

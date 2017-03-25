2165dffeef8ce9ac273655466b124d5680cb1611
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-03-25 09:42:02 -0700

    done w linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,8 +13,22 @@ function Node(value) {
     this.next = null;
 }
 
-function reverseLinkedList(head) {
-
+function reverseLinkedList(n1) {
+	var currentNode = n1;
+	let collection = [];
+	while(currentNode){
+		collection.unshift(currentNode.value)
+		currentNode = currentNode.next;
+	}
+	var backwards = new Node(collection[0]);
+	var pointer = backwards;
+	for(var i=1; i<collection.length; i += 1){
+		pointer.next = new Node(collection[i]);
+		pointer = pointer.next;
+	}
+	// return collection;
+	return backwards;
+	
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

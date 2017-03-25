0a7cf7221ee97a10e0e90c0f52bb295defc8fde8
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-03-25 10:05:30 -0700

    completed reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,26 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let headNode = head;
+    let headNodeTail = head;
+    let targetNode = head.next;
+    let headTemp = {};
+    let headTempTail = {};
+    while (targetNode !== null) {
+        headTemp = headNode; 
+        headNodeTail.next = targetNode.next;
+        targetNode.next = headTemp;
+        headNode = targetNode;
+        targetNode = headNodeTail.next;
+    }
+    return headNode;
 }
+
+// node1 = new Node(1);
+// node1.next = (new Node(2));
+// node1.next.next = (new Node(3));
+// node1.next.next.next = (new Node(4));
+// node1.next.next.next.next = (new Node(5));
+// console.log(reverseLinkedList(node1));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

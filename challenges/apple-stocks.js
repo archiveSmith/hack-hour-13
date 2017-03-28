3a1720ecb122ee6bbfed7eb00a935b01c1b9f3d4
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-28 09:41:09 -0700

    apple stocks working

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,28 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
+  if (!Array.isArray(stock_prices_yesterday)) return 0;
+  if (stock_prices_yesterday.length === 0) return 0;
 
+  let invalidInput = false;
+  let max = -Infinity;
+  let result = -Infinity;
+  //create array that stores greatest beyond that point
+  const peakPastTime = [];
+  for (i = stock_prices_yesterday.length - 1; i >= 0; i -= 1) {
+    if (stock_prices_yesterday[i] > max) max = stock_prices_yesterday[i];
+    peakPastTime.unshift(max);
+  }
+
+  if (invalidInput) return 0;
+  //create array that stores largest prof if bought at that time
+  const highestProf = peakPastTime.map((peak, ind) => peak - stock_prices_yesterday[ind]);
+  //find max of that array
+  highestProf.forEach(prof => {
+    if (prof > result) result = prof;
+  });
+
+  return result > 0 ? result : 0;
 }
 
 module.exports = bestProfit;

9abc3d06dcc5847a5a135ef34b235c6b6b4b2d81
Steven Meads <sbmeads@gmail.com>
2017-03-28 08:45:10 -0700

    complete reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -8,13 +8,31 @@
  *
  */
 
-function Node(value) {
-    this.value = value;
-    this.next = null;
-}
+ // const list = new Node(1);
+ // list.next = new Node(2);
+ // list.next.next = new Node(3);
+ // list.next.next.next = new Node(4);
+ //
+ // function Node(value) {
+ //     this.value = value;
+ //     this.next = null;
+ // }
 
-function reverseLinkedList(head) {
+ function reverseLinkedList(head) {
+   let currNode = head;
+   let prevNode;
+   let nextNode;
 
-}
+   while (currNode !== null) {
+     nextNode = currNode.next;
+     currNode.next = prevNode;
+     prevNode = currNode;
+     currNode = nextNode;
+   }
+
+   head = prevNode;
+   return head;
+ }
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

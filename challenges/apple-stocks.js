9cb1dea041e3df604f18db08a30f871692cdd9e6
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-03-29 09:20:54 -0700

    stock

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -56,7 +56,15 @@ subtract highest from smallest
 */
 
 
-
+function bestProfit2(prices) {
+  let buy = prices[0];
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
+  }
+  return profit;
+}
 
 
 

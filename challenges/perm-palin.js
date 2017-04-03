12a3cbc01a8e0f88f379a0fadb8cecb524d91a8d
Jordan Bautista <jordanbau@gmail.com>
2017-04-03 08:39:57 -0700

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,15 @@
  */
 
 function permPalin(str) {
-	
+    if (typeof str !== 'string' || str.length === 0) return false;
+    if (str.length === 1) return true;
+
+    for (let i = 1; i < str.length; i += 1) {
+        if (str[i - 1] === str[i]) return true;
+        if (str[i + 1] && str[i + 1] === str[i - 1]) return true;
+    }
+
+    return false;
 }
 
 module.exports = permPalin;

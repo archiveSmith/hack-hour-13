1420e30dd57f883952f149df087b6eaa0de1feb5
Mary-Snow <snow.mary.r@gmail.com>
2017-04-03 09:14:36 -0700

    wokred on challenges

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
-}
+  //split and rever the string. 
+  //loop string grabbing the first two values and see if that is found in the rever string
+  //if so, return true; 
+  //return false; 
 
-module.exports = permPalin;
+  if (str.length === 1) return console.log(true);
+  let reverseStr = str.split("").reverse().join("")
+  for (let j = 0; j < str.length; j++)
+    for (let i = j + 1; i <= str.length; i++) {
+      console.log(reverseStr, str.slice(0, i))
+      if (reverseStr.includes(str.slice(0, i))) return console.log(true);
+    }
+  return console.log(false);
+}
+permPalin('abab')// => true
+permPalin('cbaba') //=> true
+permPalin('cbac')// => false
+permPalin('a')// => truemodule.exports = permPalin;

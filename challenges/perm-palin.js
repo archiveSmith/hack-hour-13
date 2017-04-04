ee0b13b620338966e715d2488b82d98f3dd8bc64
Titan0723 <yu.angela.c@gmail.com>
2017-04-04 10:03:30 -0700

    hh

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -2,15 +2,32 @@
  * Given a string, determine if any of the permutations of that string is a palindrome
  *
  * Example:
- * 	- permPalin('abab') => true
- * 	- permPalin('cbaba') => true
- * 	- permPalin('cbac') => false
- * 	- permPalin('a') => true
- *
+ * 	- permPalin('abab') => true -- abba || even number of both letters
+ * 	- permPalin('cbaba') => true -- abcba || even number of both letters and 1 odd letter 
+ * 	- permPalin('cbac') => false -- bcca || even number of letters and 2 odd letters
+ * 	- permPalin('a') => true -- a
+ * 
  */
 
+// if there are a even number of letters ==> good
+// if there are a an odd number of letters ==> 1 is good, it can be in the middle
+// if there are 2+ sets of odd letters, thats bad
+// 
 function permPalin(str) {
-	
+  countObj = {};
+  stringArr = str.split("")
+  console.log(stringArr);
+  for (var i = 0; i < stringArr.length; i += 1) {
+    if (countObj.hasOwnProperty(stringArr[i])) {
+      countObj[stringArr[i]] += 1;
+    } else {
+      countObj[stringArr[i]] = 1;
+    }
+  }
+  console.log(countObj);
+  return null;
 }
+
+console.log(permPalin('cbaba'));
 
 module.exports = permPalin;

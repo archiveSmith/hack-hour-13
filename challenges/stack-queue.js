b393fd4e86e0f8363ea04ed4985cd7e0acab3351
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-04-04 09:46:17 -0700

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -3,17 +3,53 @@
  */
 
 
-function Stack() {
+class Stack {
+  constructor() {
+    this.storage = {};
+    this.index = 0;
+  }
+
+  push(value) {
+    this.storage[this.index] = value;
+    this.index += 1;
+  }
+
+  pop() {
+    if (this.index > 0) {
+      this.index -= 1;
+      const result = this.storage[this.index];
+      delete this.storage[this.index];
+      return result;
+    }
+    return undefined;
+  }
 
 }
-
 
 /**
 * Queue Class
 */
 
+class Queue {
+  constructor() {
+    this.storage = {};
+    this.index = 0;
+    this.inbox = new Stack();
+    this.outbox = new Stack();
+  }
 
-function Queue() {
+  enqueue(value) {
+    this.inbox.push(value);
+  }
+
+  dequeue() {
+    if (this.outbox.index === 0) {
+      while (this.inbox.index > 0) {
+        this.outbox.push(this.inbox.pop());
+      }
+    }
+    return this.outbox.pop();
+  }
 
 }
 

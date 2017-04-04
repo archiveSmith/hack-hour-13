753bea65555d0fdfa540a66f930e4b2408f58670
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-04 09:44:08 -0700

    Completed stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,16 @@
 
 
 function Stack() {
-
+  this.val = [];
+  this.length = 0;
+  this.push = (el) => {
+    this.val.push(el);
+    return ++this.length;
+  }
+  this.pop = () => {
+    if (this.length) this.length--;
+    return this.val.pop();
+  }
 }
 
 
@@ -14,7 +23,22 @@ function Stack() {
 
 
 function Queue() {
-
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();
+  this.length = 0;
+  this.enqueue = (el) => {
+    while (this.stack1.length) {
+      this.stack2.push(this.stack1.pop());
+    }
+    this.stack1.push(el);
+    while (this.stack2.length) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return ++this.length;
+  }
+  this.dequeue = () => {
+    return this.stack1.pop();
+  }
 }
 
 module.exports = {Stack: Stack, Queue: Queue};

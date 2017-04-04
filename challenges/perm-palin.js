487c96777f2288d70ac078db3be943703f0afde5
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-04-04 09:20:36 -0700

    rotate

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -43,32 +43,29 @@ function findPerms(string) {
   return permutations;
 }
 
-console.log( 'SOME A PALINDRONE_true', permPalin('abba') ) 
-console.log( 'SOME A PALINDRONE_true', permPalin2('abba') ) 
-console.log( 'SOME A PALINDRONE_true', permPalin('cbaba') ) 
-console.log( 'SOME A PALINDRONE_false', permPalin('cbac') ) 
-console.log( 'SOME A PALINDRONE_true', permPalin('a') ) 
+// console.log( 'SOME A PALINDRONE_true', permPalin('abba') ) 
+// console.log( 'SOME A PALINDRONE_true', permPalin2('abba') ) 
+// console.log( 'SOME A PALINDRONE_true', permPalin('cbaba') ) 
+// console.log( 'SOME A PALINDRONE_false', permPalin('cbac') ) 
+// console.log( 'SOME A PALINDRONE_true', permPalin('a') ) 
 //console.log( findPerms('abba') );
 
-//see if odd match 
-function permPalin2(str) {
-  
-    var letterCounts = {};
-    var letter;
-    var palindromeSum = 0;
-    for (var i = 0; i < str.length; i++) {
-        letter = str[i];
-        letterCounts[letter] = letterCounts[letter] || 0;
-        letterCounts[letter]++;
-    }
-    for (var letterCount in letterCounts) {
-        palindromeSum += letterCounts[letterCount] % 2;
-    }
+/////////////////////////////////////////////////////////////////
 
-    return palindromeSum < 2;
+
+//if each letter is used and even amount of times, then it is a palindrone.  If there is one that is used an odd # of times, then that's okay b/c the extra letter could be the middle number in an odd length word.  But all other cases won't allow a palindrone.
+function permPalin2(str) {
+  let letterCount = {};
+
+  for (let i = 0; i < str.length; i += 1) {
+    letterCount[str[i]] ? delete letterCount[str[i]] : letterCount[str[i]] = 1
+  }
+
+  return Object.keys(letterCount).length <= 1;  
+
 }
 
-
+console.log( permPalin2('abbaba') )
 
 
 

b61af46ba48dd1243b32f75f5ce995a00f02e432
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-04 09:55:57 -0700

    stack-queue and rotate-grid

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,21 @@
 
 
 function Stack() {
-
+  this.value = {};
+  this.length = 0;
+  this.push = (el) => {
+    this.value[this.length] = el;
+    this.length += 1;
+  };
+  this.pop = () => {
+    if (this.length) {
+      let result = this.value[this.length - 1];
+      delete this.value[this.length - 1];
+      this.length -= 1;
+      return result;
+    }
+    return undefined;
+  };
 }
 
 
@@ -14,7 +28,27 @@ function Stack() {
 
 
 function Queue() {
-
+  const enStack = new Stack();
+  const deStack = new Stack();
+  this.length = deStack.length;
+  this.enqueue = (el) => {
+    for (let i = 0; i < this.length; i += 1) {
+      enStack.push(deStack.pop());
+    }
+    enStack.push(el);
+    for (let i = 0; i <= this.length; i += 1) {
+      deStack.push(enStack.pop());
+    }
+    this.length = deStack.length;
+  }
+  this.dequeue = () => {
+    if (this.length) {
+      let result = deStack.pop();
+      this.length = deStack.length;
+      return result;
+    }
+    return undefined;
+  }
 }
 
 module.exports = {Stack: Stack, Queue: Queue};

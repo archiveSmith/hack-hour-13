657cd581696d9ab47fdbadd95316e18ac2eab7b0
Jordan Bautista <jordanbau@gmail.com>
2017-04-04 09:44:29 -0700

    rotate grid + stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,16 @@
 
 
 function Stack() {
+    this.contents = [];
+}
 
+
+Stack.protoype.push = (a) => {
+    this.contents.push(a);
+}
+
+Stack.prototype.pop = () => {
+    return this.contents.pop();
 }
 
 
@@ -13,8 +22,33 @@ function Stack() {
 */
 
 
-function Queue() {
+function Queue(stack1, stack2) {
+    this.contents = [];
 
+    let nextIs1 = true;
+    while (stack1 && stack2) {
+        if (nextIs1) Queue.push(stack1.pop());
+        else Queue.push(stack2.pop());
+    }
+
+    if (stack1) {
+        while (stack1) {
+            Queue.push(stack1.pop());
+        }
+    } else {
+        while (stack2) {
+            Queue.push(stack2.pop());
+        }
+    }
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+Queue.prototype.push = (a) => {
+    this.contents.push(a);
+}
+
+Queue.prototype.unshift = () => {
+    return this.contents.unshif();
+}
+
+
+module.exports = { Stack: Stack, Queue: Queue };

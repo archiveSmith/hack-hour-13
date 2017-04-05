722939a2dd93013f21daa1c7ecd07410e756a512
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-05 09:24:14 -0700

    two sum working

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,11 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
 function twoSum(arr, n) {
-
+  if (n === 0) return true;
+  if (!arr.length) return false;
+  return twoSum(arr.slice(1), n) || arr.slice(1).includes(n - arr[0]);
 }
 
 module.exports = twoSum;

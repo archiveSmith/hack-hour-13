e11cd7bd8a4af7621352252c87332ff05499276e
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-04-05 09:27:16 -0700

    christian done with two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,16 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
-
+function twoSum(arr, target) {
+	if(arr.length < 2)  return false;
+	var sum=0;
+	for(var i = 0; i < arr.length - 1; i += 1){
+		for(var j=i+1; j<arr.length; j += 1){
+			if(arr[i] + arr[j] === target)
+			return true
+		}	
+	}
+	return false;
 }
 
 module.exports = twoSum;

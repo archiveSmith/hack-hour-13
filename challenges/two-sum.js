52022f107586218a1d351bac3c5986c9a4b621d7
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-05 09:57:19 -0700

    finished two Sum, still working on recursion

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,34 @@
  */
 
 function twoSum(arr, n) {
+  // recursive
 
+  // with two for loops
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i] + arr[j] === n) {
+        return true;
+      }
+    }
+  }
+  return false;
+
+
+  // attempt with one for loop ~~~ doensnt fully work cuz deep clone not working
+  // let copyArr = arr;
+
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   if (copyArr[copyArr.length - 1] + arr[i] === n && i !== copyArr.length - 1) {
+  //     return true;
+  //   } else if (i === arr.length - 1) {
+  //     if (copyArr.length > 0) {
+  //       copyArr.pop();
+  //       i = 0;
+  //     }
+  //   }
+  // }
+
+  // return false;
 }
 
 module.exports = twoSum;

fd620f17739c30b0f244497ad9c477d7033f0390
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-04-06 15:05:27 -0700

    valid bst 6th try

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -11,21 +11,29 @@ function BinaryTree(val) {
   this.right = null;
 }
 
-function validBST(tree) {
-  const tr = tree;
-  const val = tr.value;
-  return (function check(BST) {
-    if (BST === null) return true;
-    if (BST.value === val) {
-      if ((BST.left !== null && BST.left.value > BST.value) ||
-        (BST.right !== null && BST.right.value < BST.value)) return false;
-    }
-    if (BST.value !== val) {
-      if ((BST.left !== null && (BST.left.value > BST.value || BST.right.value > val)) ||
-        (BST.right !== null && (BST.right.value < BST.value || BST.left.value < val))) return false;
-    }
-    return check(BST.left) && check(BST.right);
-  }(tr));
+// function validBST(tree) {
+//   const tr = tree;
+//   const val = tr.value;
+//   return (function check(BST) {
+//     if (BST === null) return true;
+//     if (BST.value === val) {
+//       if ((BST.left !== null && BST.left.value > BST.value) ||
+//         (BST.right !== null && BST.right.value < BST.value)) return false;
+//     }
+//     if (BST.value !== val) {
+//       if ((BST.left !== null && (BST.left.value > BST.value || BST.right.value > val)) ||
+//         (BST.right !== null && (BST.right.value < BST.value || BST.left.value < val))) return false;
+//     }
+//     return check(BST.left) && check(BST.right);
+//   }(tr));
+// }
+
+function validBST(tree, val) {
+  if (val === undefined) val = tree.value;
+  if (tree === null) return true;
+  if ((tree.left !== null && tree.left.value > val) ||
+    (tree.right !== null && tree.right.value < val)) return false;
+  return validBST(tree.left, tree.left.value) && validBST(tree.right, tree.right.value);
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};

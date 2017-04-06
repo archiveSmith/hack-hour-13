09ad3da60919e74865721aae161af04c969a489c
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-04-06 09:55:39 -0700

    valid BST working

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,7 +3,7 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -11,8 +11,48 @@ function BinaryTree(val) {
     this.right = null;
 }
 
-function validBST(tree) {
+// function validBST(tree, min, max) {
+//   console.log('min < tree value < max:', min + ' < ' + tree.value + ' < ' + max);
+//   if (tree === null) return true;
+//   if (tree.value < min || tree.value >= max) return false;
+//
+//   let leftValid = true;
+//   let rightValid = true;
+//
+//   if (tree.left && tree.left.value > tree.value) leftValid = false;
+//   if (tree.right && tree.right.value <= tree.value) rightValid = false;
+//
+//   return leftValid && rightValid && validBST(tree.left, min, tree.value) && validBST(tree.right, tree.value, max);
+// }
 
+function validBST(tree, min, max) {
+  if (tree === null) return true;
+
+  if (min === undefined) min = -Infinity;
+  if (max === undefined) max = Infinity;
+
+  if (tree.value <= min || tree.value > max) return false;
+
+  return validBST(tree.left, min, tree.value) && validBST(tree.right, tree.value, max);
 }
+
+// const root = new BinaryTree(0);
+// const left = new BinaryTree(-2);
+// const right = new BinaryTree(2);
+// const leftRight = new BinaryTree(0);
+// const leftLeft = new BinaryTree(-3);
+// const rightLeft = new BinaryTree(1.5);
+// const rightRight = new BinaryTree(3);
+//
+// root.left = left;
+// root.right = right;
+//
+// left.left = leftLeft;
+// left.right = leftRight;
+//
+// right.left = rightLeft;
+// right.right = rightRight;
+//
+// console.log(validBST(root));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};

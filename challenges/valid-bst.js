285ae8c05c32f11b29f51121a567710784167b2d
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-04-06 16:29:36 -0700

    attempt at valid-bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,6 +12,22 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+    // let middle = tree.value;
+    // let leftPointer = tree.left, rightPointer = tree.right;
+
+    // while (tree.value === null || leftPointer === null && rightPointer === null) {
+    //     //check leftside is bigger or right side is smaller, and also do same logic comparison with root of the tree.
+    //     if (leftPointer.left.value > leftPointer.value && leftPointer.value > middle &&
+    //         rightPointer.right.value <= rightPointer.value && rightPointer.value <= middle) {return false};
+    //     leftPointer = leftPointer.left;
+    //     rightPointer = rightPointer.right;
+    // }
+    // return true;
+
+    if (tree.value === null || tree.left === null && tree.right === null) return true;
+
+    return validBST(this.left <= this.value && this.left <= tree.value) && 
+           validBST(this.right > this.value && this.right > tree.value)
 
 }
 

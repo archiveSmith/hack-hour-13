0effda2ecf25d04d6168a82be8efcd59f2ff4cf3
John C <John@Johns-MacBook-Air.local>
2017-04-07 09:24:29 -0700

    first gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,13 @@
  */
 
 function gcd(a, b) {
-
+  let maxDivisor = 1;
+  // iterate through a, and determine which is the highest number between a and b
+  // that will return 0 for mod n
+  for (let i = 0; i < a; i += 1) {
+    if (a % i === 0 && b % i === 0) maxDivisor = i;
+  }
+  return maxDivisor;
 }
 
 module.exports = gcd;

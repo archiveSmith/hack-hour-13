2c4c14d6c95ff223bce0033a453c88880e151a4d
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-04-07 09:42:27 -0700

    completed gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,16 @@
  */
 
 function gcd(a, b) {
-
+  if (typeof a !== 'number' || typeof b !== 'number') return 'please enter numbers only'
+  if (a === b) return a;
+  let max = Math.max(a, b);
+  let min = Math.min(a, b);
+  if (max % min === 0) return min;  
+  let divisor = Math.floor(min/2)
+  while (max % divisor !== 0 || min % divisor !== 0) {
+    divisor -= 1;
+  }
+  return divisor;
 }
 
 module.exports = gcd;

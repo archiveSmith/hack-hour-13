20762b1d013df67794bd226da9d98d2ff81a4c9c
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-07 09:29:25 -0700

    gcd success

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,18 @@
  */
 
 function gcd(a, b) {
+  if (typeof a !== number || typeof b !== number) return false;
 
+  let min = Math.min(a, b);
+  let divisor = 1;
+
+  for (let i = 2; i <= min; i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      divisor = i;
+    }
+  }
+
+  return devisor;
 }
 
 module.exports = gcd;

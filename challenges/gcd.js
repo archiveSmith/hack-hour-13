17d397bc5691194d48a51837f8fc39e42fbb32b4
Kangseon Cho <kangseoncho@Kangseons-MacBook-Air.local>
2017-04-07 09:38:12 -0700

    finished gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,21 @@
  */
 
 function gcd(a, b) {
+  let aDivisors = [], bDivisors = [];
 
+  for (let i = 1; i <= a; i += 1) {
+      if (a % i === 0) aDivisors.push(i);
+  }
+    
+   for (let i = 1; i <= b; i += 1) {
+      if (b % i === 0) bDivisors.push(i);
+  }
+
+  let commonDivisors = aDivisors.filter((element, index) => bDivisors.indexOf(element) !== -1);
+
+  if (commonDivisors.length === 0) return 1;
+  
+  return commonDivisors[commonDivisors.length-1];
 }
 
 module.exports = gcd;

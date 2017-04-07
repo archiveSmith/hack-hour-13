23ed0497d0abe4d3f85b7f300285340a27d7f526
Johnny Coyle <johnnycoyle@gmail.com>
2017-04-07 12:03:38 -0700

    hack-hour gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,6 +9,17 @@
 
 function gcd(a, b) {
 
+  if (isNaN(a+b)) return 'invalid inputs. please provide two numbers';
+
+  if (Math.max(a, b) === b) b = [a, a = b][0];
+
+  let divisor = b;
+  while (divisor > 1) {
+    if (a % divisor === 0 && b % divisor =q== 0) return divisor;
+    divisor--;
+  }
+  return b;
+
 }
 
 module.exports = gcd;

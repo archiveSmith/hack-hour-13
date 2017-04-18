2277262cb76e165ce5c59b819b69fe6bfcce1aa2
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-18 07:04:21 -0700

    Brute-force count twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -5,10 +5,27 @@
 // countTwos(13);  -> 2
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
-
+'use strict';
 
 function countTwos(num) {
+  // only positive ints
+  // if less than 2, no 2s
+  if (num < 2 || num % 1) return 0;
 
+  // check for num of 2s from 1 to num
+  let twoCount = 0;
+  for (let i = 2; i <= num; i += 1) {
+    // count digits that are 2
+    twoCount += i.toString().split('').reduce((numTwos, digit) => numTwos += (digit === '2'), 0);
+  }
+
+  return twoCount;
 }
+
+console.log(countTwos(1));
+console.log(countTwos(3));
+console.log(countTwos(13));
+console.log(countTwos(1000));
+console.log(countTwos(11420));
 
 module.exports = countTwos;

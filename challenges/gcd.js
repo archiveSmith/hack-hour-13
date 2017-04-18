333c1608610c7952bb6ef8236e9ddfd8b7683f0a
mkhouv <mkhouv@gmail.com>
2017-04-18 09:50:21 -0700

    completed apply it

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,15 +7,21 @@
  *
  */
 
-function gcd(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') return 'Invalid input';
-  let curr = 1;
-  let max;
-  while (curr <= b) {
-    if (a % curr === 0 && b % curr === 0) max = curr;
-    curr += 1
-  }
-  return max;
-}
+// function gcd(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') return 'Invalid input';
+//   let curr = 1;
+//   let max;
+//   let higherNum;
+//   if (a > b) higherNum = a;
+//   else higherNum = b;
+//   while (curr <= higherNum) {
+//     if (a % curr === 0 && b % curr === 0) max = curr;
+//     curr += 1
+//   }
+//   return max;
+// }
+
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+console.log(gcd(80, 160));
 
 module.exports = gcd;

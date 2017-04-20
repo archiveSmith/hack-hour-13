b9364aae55a571efc1d8195df12c0de832d0e953
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-04-20 09:59:43 -0700

    completed superbalanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,32 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  return !tree || Math.abs(treeHeight(tree.left) - treeHeight(tree.right)) <= 1;
+}
 
+function treeHeight(tree, height = 0) {
+  return tree ? Math.max(treeHeight(tree.left, height + 1), treeHeight(tree.right, height + 1)) : height;
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+
+
+// let five = new BinaryTree(5);
+// let two = new BinaryTree(2);
+// let one = new BinaryTree(1);
+// let four = new BinaryTree(4);
+// let three = new BinaryTree(3);
+// let six = new BinaryTree(6);
+// let seven = new BinaryTree(7);
+
+
+// five.left = two;
+// five.right = six;
+// six.right = seven;
+// five.left = two;
+// two.left = one;
+// two.right = four;
+// four.left = three;
+
+
+// console.log(superbalanced(five));

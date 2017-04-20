f49adda78b252b905e8485bc0ab3843303b41d5f
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-20 09:09:26 -0700

    wokring on cide

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,31 +26,54 @@
  */
 
 function applyIt(func, args) {
-  // just pass args into the function
-  let thefunc = func.toString();
-  let callback = '';
+  // build up the func call as a string and eval it
+  let funcStr = 'func(';
 
-  // grab only the function name
-  for (let i = 9; thefunc[i] !== '('; i += 1) {
-    callback += thefunc[i];
-  }
+  // create an array containing each element of the arg list (arg[0] through arg[n])
+  const argList = args.map((element, i) => {
+    // cannot do return `${element}` because 'South Carolina' 
+    // will have to do toString for strings so that it keeps the space as well as the quotes
+    //        return (typeof element === 'string') ? `${element.toString()}` : `${element}`;
+    return `args[${i}]`;
 
-  // add the opening parens to the callback
-  callback += '(';
 
-  // convert arguments to string, and add to the string
-  for (let i = 0; i < args.length; i += 1) {
-    // if it is the first arg
-    if (i === args.length - 1) {
-      callback += args[i];
-      callback += ')';
-    } else {
-      callback += `${args[i]},`;
-    }
-  }
+  }) 
+
+  // put all the arguments together and close out the parens
+  funcStr += argList.join(',') + ');'
 
   // eval the function
-  return () => eval(callback);
+  return () => eval(funcStr);
 }
+
+// this is the correct method! Yay! :D, it just doesn't fully work yet
+// function applyIt(func, args) {
+//   // just pass args into the function
+//   let thefunc = func.toString();
+//   let callback = '';
+
+//   // grab only the function name
+//   for (let i = 9; thefunc[i] !== '('; i += 1) {
+//     callback += thefunc[i];
+//   }
+
+//   // add the opening parens to the callback
+//   callback += '(';
+
+//   // convert arguments to string, and add to the string
+//   for (let i = 0; i < args.length; i += 1) {
+//     // if it is the first arg
+//     if (i === args.length - 1) {
+//       callback += args[i];
+//       callback += ')';
+//     } else {
+//       callback += `${args[i]},`;
+//     }
+//   }
+
+//   // eval the function
+//   return () => eval(callback);
+// }
+
 
 module.exports = applyIt;

30cf5e3080544974ac0f9239a11bde608400a8d4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-21 09:55:01 -0700

    Repeat nums - O(n) time and space

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -9,9 +9,18 @@
  * Do this in O(1) space
  *
  */
+'use strict';
 
-function repeatNumbers(array) {
+// assuming passed in array follows array laid out by description
 
+// O(n) time, O(n) space
+function repeatNumbers1(array) {
+  const seen = {};
+  for (let num of array) {
+    // store seen in object for O(1) lookup so that O(1) for each num makes it O(n) overall
+    if (seen[num]) return num;
+    else seen[num] = true;
+  }
 }
 
-module.exports = repeatNumbers;
+module.exports = repeatNumbers1;

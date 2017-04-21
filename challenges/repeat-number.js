e51594a4da715ba6fbfdd88c9ab7c4560ec8fa66
Titan0723 <yu.angela.c@gmail.com>
2017-04-21 10:28:27 -0700

    hh

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,23 @@
  */
 
 function repeatNumbers(array) {
-
+  array.sort();
+  var maxVal = array[array.length - 1];
+  var sum1 = 0;
+  var sum2 = 0;
+  for (i = 1; i <= maxVal; i += 1) {
+    sum1 += i;
+  }
+  // console.log(sum1);
+  var sum = array.reduce((sum2, element) => {
+    return sum2 + element;
+  }, 0);
+  // console.log(sum);
+  var repeatNo = sum - sum1;
+  return repeatNo;
 }
+
+// var testArr = [1,2,2,3,4];
+// console.log(repeatNumbers(testArr));
 
 module.exports = repeatNumbers;

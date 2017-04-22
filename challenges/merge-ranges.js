7b22175f9717f3ce295095b1a7b9818049b40f3a
Jeffrey Ma <jjeffreyma@gmail.com>
2017-04-22 09:40:45 -0700

    Initial approach

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,24 @@
 
 
 function mergeRanges(array) {
-
+  array.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  let result = [[0, 0]];
+  for (let i = 0; i < array.length - 1; i += 1) {
+    let lower = array[i][0];
+    let upper = array[i][1];
+    if (result[result.length - 1][1] >= lower) {
+      lower = result[result.length - 1][0];
+      result.pop();
+    }
+    if (upper >= array[i + 1][0] && upper < array[i + 1][1]) {
+      upper = array[i + 1][1];
+      i += 1;
+    }
+    result.push([lower, upper]);
+  }
+  return result;
 }
 
 module.exports = mergeRanges;

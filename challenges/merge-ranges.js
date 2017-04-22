c5aede9f19e1536bf307515362bc3e43ce11a836
Kangseon Cho <kangseon.cho@gmail.com>
2017-04-22 09:52:15 -0700

    1st attemp at merge-range

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,21 @@
 
 
 function mergeRanges(array) {
+  let vessel = array.sort((a,b) => a[0] - b[0]);
 
+  for(let i = 0; i < vessel.length; i += 1) {
+    //while(vessel[i][1] >= vessel[i+1][0]) {
+      if(vessel[i+1] === undefined) {
+        return vessel;
+      } else if(vessel[i][1] >= vessel[i+1][0]) {
+        //if there is overlap, then merge and push to vessel
+        let newTime = [vessel[i][0],vessel[i+1][1]]
+        vessel[i] = newTime;
+        vessel.splice(i+1, 1)
+      }
+    //}
+  }
+  return vessel
 }
 
 module.exports = mergeRanges;

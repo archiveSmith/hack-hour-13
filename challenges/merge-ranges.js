d2c34b03379add64ce924742989d3f5c284e9120
slhstudio <slhstudio@gmail.com>
2017-04-22 09:46:23 -0700

    done

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,17 @@
 
 
 function mergeRanges(array) {
-
+  let sorted = array.sort((a, b) => a[0] - b[0]);
+  let ranges = []
+  for(let i = 0; i < sorted.length - 1; i += 1) {
+    if ((sorted[i][0] < sorted[i + 1][0]) && (sorted[i][1] < sorted[i + 1][1]) && (sorted[i][1] >= sorted[i + 1][0])) {
+      let range = [sorted[i][0], sorted[i + 1][1]];
+      ranges = ranges.concat([range]);
+      i += 1;
+    } else
+       ranges = ranges.concat([sorted[i]]);
+    }
+    return ranges;
 }
 
 module.exports = mergeRanges;

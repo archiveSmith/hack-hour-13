4dd3adf7f9059bf119088ee27c9021fba6f3b8f0
Steven Meads <sbmeads@gmail.com>
2017-04-24 09:45:05 -0700

    attempt one at linked list cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,13 +27,36 @@
  *
  */
 
+// Psuedocode 0(n) time 0(n) space
+// create an empty object to store each value of the linked list
+// iterate through linked list and add values to cache object
+  // each iteration check currNode's value is in the cache.
+  // if in cache return true, otherwise return false.
+
 var Node = function(value) {
   this.value = value;
   this.next = null;
 }
 
 function hasCycle(head) {
+  const cache = {};
+  let currentNode = head;
 
+  while (currentNode) {
+    if (cache[currentNode.value]) return true;
+    cache[currentNode.value] = true;
+    currentNode = currentNode.next;
+  }
+  return false;
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// hasCycle(node1); // => false
+// node5.next = node2;
+// hasCycle(node1); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}

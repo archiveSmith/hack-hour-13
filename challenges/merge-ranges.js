0ed9b08e927b90b56232a51bc34b9eb7f5f4ada4
Titan0723 <yu.angela.c@gmail.com>
2017-04-24 09:40:48 -0700

    hh

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,31 @@
  * Do not assume the ranges are in order
  */
 
-
 function mergeRanges(array) {
-
+  var storageObj = {};
+  array.forEach(range => {
+    for (var i = range[0]; i < range[1]; i++) {
+      storageObj[i] = true;
+    }
+  });
+  var storageArr = Object.keys(storageObj);
+  storageArr.sort();
+  var returnArr = [];
+  var tempRange = [];
+  for (var j = 0; j < storageArr.length; j++) {
+    if (j === 0) {
+      tempRange[0] = storageArr[j];
+    } else if (storageArr[j] !== storageArr[j - 1] - 1) {
+      tempRange[0] = storageArr[j];
+    }
+    if (tempRange[0]) {
+      storageArr[j]
+    }
+    storageArr[i];
+  }
+  return "woo";
 }
 
+var testArr = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+console.log(mergeRanges(testArr));
 module.exports = mergeRanges;

85b0d41d4bfa1f5f4b27e5f4463a2e90b2b736c0
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-25 09:21:10 -0700

    Multiples of x or y in z

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -5,17 +5,27 @@
 // write a function that will find the sum of all the multiples of 3 or 5
 // below 1000 and return that sum.
 
-function sumMultiples3Or5Below1000() {
-  let sum = 0;
-
-  return sum;
-}
-
+const sumMultiples3Or5Below1000 = () => sumMultiplesXOrYBelowZ(3,5,1000);
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
+  if (
+    // non-numbers, nonpositive numbers, or non-integers not allowed
+    arguments.some(arg => isNaN(arg) || arg <= 0 || arg % 1)
+  ) {
+    throw Error('Invalid argument(s)');
+  }
+
   let sum = 0;
+
+  // iterate up to z
+  for (let num = 1; num < z; num += 1) {
+    // if num is either a multiple of x or y
+    if (!(num % x) || !(num % y)) {
+      sum += num;
+    } 
+  }
 
   return sum;
 }

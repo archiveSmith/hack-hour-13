561d6eddfce2a211a02d89bf3bee22db52a55336
Jeffrey Ma <jjeffreyma@gmail.com>
2017-04-25 09:22:39 -0700

    update

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,7 +7,7 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-  for (let i = 0; i < 16; i += 1) {
+  for (let i = 0; i < 1000; i += 1) {
     if (i % 3 === 0) {
       sum += i;
       continue;

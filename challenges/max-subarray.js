4882619cc598e282e13b539a03fa4c6bb8592faa
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-26 09:47:15 -0700

    finished max sub array

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -14,9 +14,15 @@ function maxSubarray(arr) {
 
   let maxSum = 0;
   let currSum = 0;
+  let negative = true;
 
   // at any point where the values added up to that point are not negative, is a positive case
   for (let i = 0; i < arr.length; i += 1) {
+    // if we find a positive number, then everything u know is a lie
+    if (arr[i] >= 0) {
+      negative = false;
+    }
+
     currSum += arr[i];
     if (currSum < 0) {
       currSum = 0;
@@ -26,11 +32,17 @@ function maxSubarray(arr) {
     }
   }
 
+  // but what if all the numbers are negative?
+  if (negative) {
+    return Math.max(...arr)
+  }
   return maxSum;
 }
 
-console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
-console.log(maxSubarray([15,20,-5,10]));
-console.log(maxSubarray([5,5,-9,5,6]));
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+// console.log(maxSubarray([15,20,-5,10]));
+// console.log(maxSubarray([5,5,-9,5,6]));
+// console.log(maxSubarray([-5,-5,-9,-5,-6]));
 
 module.exports = maxSubarray;

557fc3f8eec9c7418523cdb1121d72bfd7d047c8
Steven Meads <sbmeads@gmail.com>
2017-04-26 10:32:53 -0700

    attempt one at max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,29 @@
  *
  */
 
-function maxSubarray(arr) {
+// input -> an array of integers both pos and neg.
+// output -> the max sum of all subarrays
+// have a result variable to hold max
+// loop through input array
+// nested loop that will sum current index with all other indexes to get value
+  // store curr max as result
+  // if curr max is greater than result overwrite
 
+function maxSubarray(arr) {
+  if (!Array.isArray(arr)) return 'Input is not an array';
+  let maxSum = 0;
+  let currSum = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    currSum = currSum + arr[i];
+    if (currSum < 0) {
+      currSum = 0;
+    }
+    if (maxSum < currSum) {
+      maxSum = currSum;
+    }
+  }
+  return maxSum;
 }
 
 module.exports = maxSubarray;

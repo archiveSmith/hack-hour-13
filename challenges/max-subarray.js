ab38697072825f7f042c84e1d7aab83e40447238
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-26 09:34:43 -0700

    finished max subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,29 @@
  */
 
 function maxSubarray(arr) {
+  if (arr.constructor !== Array) {
+    return false;
+  }
 
+  let maxSum = 0;
+  let currSum = 0;
+
+  // at any point where the values added up to that point are not negative, is a positive case
+  for (let i = 0; i < arr.length; i += 1) {
+    currSum += arr[i];
+    if (currSum < 0) {
+      currSum = 0;
+    }
+    if (maxSum < currSum) {
+      maxSum = currSum;
+    }
+  }
+
+  return maxSum;
 }
+
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+console.log(maxSubarray([15,20,-5,10]));
+console.log(maxSubarray([5,5,-9,5,6]));
 
 module.exports = maxSubarray;

c9a24f8e42a0dedaf6435e91080e300afb60a6e4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-04-27 09:58:20 -0700

    Anagrams initial

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,35 @@
   */
 
 function anagrams(string) {
+  // base case as well as accounting for empty string
+  if (string.length <= 1) {
+    return [string];
+  }
+  
+  // list of permutations
+  const permutations = [];
 
+  // first letter of permutation branches
+  for (let first = 0; first < string.length; first += 1) {
+    const firstChar = string[first];
+    
+    // new string w/o firstChar
+    const otherChars = string.slice(0, first) + string.slice(first + 1);
+    
+    // permutations using other chars after first letter
+    const otherPerms = anagrams(otherChars);
+    
+    // permutations now with first char
+    const permsWithFirstChar = otherPerms.map(perm => firstChar + perm);
+    
+    // add to permutations
+    permutations.push(...permsWithFirstChar);
+  } 
+
+  return permutations;
 }
+
+// note: num of permutations = n!, where n = string length
+// time complexity may also be n!
 
 module.exports = anagrams;

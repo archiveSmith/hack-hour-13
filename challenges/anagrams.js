d4c81ae2dfe7f31e4c77c5f6d8043e965d3cb598
Jordan Bautista <jordanbau@gmail.com>
2017-04-27 09:59:47 -0700

    update 2

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -23,8 +23,10 @@ function anagrams(string) {
 
   let result = [];
 
-  if(string.length === 0) return result;
+  result.push(string);
   
+  if(string.length === 0) return result;
+
   string = string.split('');
 
   for (let i = 0; i < string.length; i += 1) {

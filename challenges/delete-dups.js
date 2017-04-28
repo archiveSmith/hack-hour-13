d0bfa1c023bb738dd1d001775cdf6d3e041639d7
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-04-28 13:48:12 -0700

    delete-deups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,32 @@
 
 
 function deleteDups(head) {
-
+	// loop thru the linked list
+	// create a map that will allow your to check for repititions
+	// if the current nodes value is in the map then delete the current Node
+	let currentNode = head;
+	let counter = {};
+	let dupNum;
+	while(currentNode){
+		if(currentNode.value in counter){
+			dupNum = currentNode.value;
+			counter[currentNode.value] ++;
+		}
+		else{
+				counter[currentNode.value] = 1;
+		}
+		currentNode = currentNode.next;
+	}
+	// get the key
+	currentNode = head;
+	while(currentNode){
+		if(currentNode.value === dupNum){
+			currentNode = currentNode.next.next;
+			break;
+		}
+		currentNode = currentNode.next;
+	}
 }
+
 
 module.exports = deleteDups;

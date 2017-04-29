f3755e9380eb3dbe6b569b5875fc0d097dd3c45b
Kevin Qiu <kami.kevinq@gmail.com>
2017-04-29 09:42:25 -0700

    event-emitter

---
+++ challenges/event-emitter.js
@@ -0,0 +1,43 @@
+'use strict';
+/**
+ * Make an EventEmitter that
+ *
+ * Example:
+ * var instance = new EventEmitter();
+ * var counter = 0;
+ * instance.on('increment', function() {
+ *   counter++;
+ * }); // counter should be 0
+ * instance.trigger('increment'); // counter should be 1
+ * instance.trigger('increment'); // counter should be 2
+ *
+ *
+ * Caveats:
+ * - If we repeatedly call .on with the same event name, it should
+ *   continue to call the old function(s) as well. That is to say, we can have multiple
+ *   listeners for one event.
+ * - If `obj.trigger` is called with additional arguments, pass those to the
+ *   listeners.
+ * - It is not necessary to write a way to remove listeners.
+ */
+
+function EventEmitter() {
+  this.bindEvent = {};
+}
+
+// should be able to run multiple functions for one event
+EventEmitter.prototype.on = function(funcName, func) {
+  // check to see if the event already exists, if it does, simply just push into it
+  if (this.bindEvent[funcName]) {
+    this.bindEvent[funcName].push(func);
+  } else {
+    this.bindEvent[funcName] = [func];
+  }
+}
+
+EventEmitter.prototype.trigger = function(funcName, ...args) {
+  this.bindEvent[funcName].forEach(listener => listener(...args))
+};
+
+
+module.exports = EventEmitter;

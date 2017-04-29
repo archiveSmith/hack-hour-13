b0d6a9e72a052580e8d35b3ab86dd28db9d013d8
Mary-Snow <snow.mary.r@gmail.com>
2017-04-29 09:37:46 -0700

    updated anagrams with solution

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,40 +12,56 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-  const permutator = (inputArr) => {
-  let result = [];
+//   const permutator = (inputArr) => {
+//   let result = [];
 
-  const permute = (arr, m = []) => {
-    if (arr.length === 0) {
-      result.push(m)
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), m.concat(next))
-     }
-   }
- }
+//   const permute = (arr, m = []) => {
+//     if (arr.length === 0) {
+//       result.push(m)
+//     } else {
+//       for (let i = 0; i < arr.length; i++) {
+//         let curr = arr.slice();
+//         let next = curr.splice(i, 1);
+//         permute(curr.slice(), m.concat(next))
+//      }
+//    }
+//  }
 
- permute(inputArr)
+//  permute(inputArr)
 
- return result;
-}
-function anagrams(string) {
-  let result = []; 
-  if(string.length < 2) return string; 
-  function myFunc()
-  for(let i =0; i < string.length; i++){
-    let char = string[i]
+//  return result;
+// }
+// function anagrams(string) {
+//   let result = []; 
+//   if(string.length < 2) return string; 
+//   function myFunc()
+//   for(let i =0; i < string.length; i++){
+//     let char = string[i]
  
-    let remainingString = string.slice(0,i) + string.slice(i + 1, string.length);
+//     let remainingString = string.slice(0,i) + string.slice(i + 1, string.length);
 
-    anagrams(remainingString)
-      result.push(char + remainingString)
-  }
+//     anagrams(remainingString)
+//       result.push(char + remainingString)
+//   }
   
-  return console.log("this", result);
-}
+//   return console.log("this", result);
+// }
 
-anagrams("abc");
+// anagrams("abc");
+
+
+function anagrams(string) {
+  const arr = string.split('');
+  function recurse(letters) {
+    let results = [];
+    if (letters.length <= 1) return results.concat(letters);
+    for (let i = 0; i < letters.length; i += 1) {
+      const front = letters[i];
+      const restOfLetters = letters.slice(0, i).concat(letters.slice(i + 1));
+      results = results.concat(recurse(restOfLetters).map(str => front+str));
+    }
+    return results;
+  }
+  return recurse(arr);
+}
 module.exports = anagrams;

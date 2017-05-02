b283be142cae06e90378f7efd82d1e89411d8d94
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-05-02 09:36:50 -0700

    completed insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,17 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
-function insertionSort(array) {
-
+function insertionSort(array, delimiter = 1) {
+  if (delimiter === array.length) return array;
+  const target = array.splice(delimiter, 1)[0];
+  for (let i = 0; i < delimiter; i += 1) {
+    if (target < array[i]) {
+      array.splice(i, 0, target);
+      return insertionSort(array, delimiter + 1);
+    }
+  }
+  array.splice(delimiter, 0, target);
+  return insertionSort(array, delimiter + 1);
 }
 
 module.exports = insertionSort;

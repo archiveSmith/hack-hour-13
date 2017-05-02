7ba2a288426bfb0d21aca3bfe1f7b54894b78359
Justin Pierson <justinpierson@Justins-MacBook-Pro.local>
2017-05-02 09:23:20 -0700

    hack hour

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,21 +10,44 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
-
 function deleteDups(head) {
-  let arr = [];
-  if (!this.head) return undefined;
-  for (let i = head; i; i = i.next) {
-    if (i === head) {
-      arr.push(i.value);
-    }
-    if (arr.includes(i.next.value)) {
-      i.next = i.next.next;
-    }
-    arr.push(i.next.value);
-  }
-  return head;
+  // let arr = [];
+  // if (!head) return undefined;
+  // for (let i = head; i; i = i.next) {
+  //   if (i === head) {
+  //     arr.push(i.value);
+  //   }
+  //   if (arr.includes(i.next.value)) {
+  //     i.next = i.next.next;
+  //   }
+  //   arr.push(i.next.value);
+  // }
+  // return head;
+
+  // const cache = {};
+  // let j;
+
+  // for (let i = head; i; i=i.next) {
+  //   if (!cache[i.value]) {
+  //     cache[i.value] = true;
+  //     j = 1;
+  //   } else {
+  //     j.next = i.next;
+  //   }
+  // }
+  // return head;
+
+  // for (let i = head; i; i = i.next) {
+  //   let j = i;
+  //   while(j.next) {
+  //     if (j.next.value) {
+  //       j.next = j.next.next;
+  //     } else {
+  //       j = j.next;
+  //     }
+  //   }
+  // }
+  // return head;
 }
 
 module.exports = deleteDups;

64e9c42f3d9011ab0aff0a5523788d789a477d3a
John C <John@Johns-MacBook-Air.local>
2017-05-02 09:25:26 -0700

    insertion-sort pt1

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
-
+  // iterate through the array, and move the smallest number in the array to
+  // the very beginning of the array
+  // do this for as many times of the array length
+  let smallNum = Infinity;
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i; j < array.length; j += 1) {
+      if (smallNum > array[j]) {
+        smallNum = array[j];
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    smallNum = Infinity;
+  }
+  return array;
 }
 
 module.exports = insertionSort;

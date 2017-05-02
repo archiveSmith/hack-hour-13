10645c32292169988017c029e346b371e07f3b40
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-02 09:32:17 -0700

    completed insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,22 @@
 
 
 function insertionSort(array) {
-
+  let output = [];
+  while (array.length) {
+    const val = array.shift()
+    if (!output.length) output.push(val);
+    else {
+      let inserted = false;
+      for (let i = 0; i < output.length; i += 1) {
+        if (val < output[i]) {
+          output.splice(i, 0, val);
+          inserted = true;
+          break;
+        }
+      }
+      if (!inserted) output.push(val);
+    }
+  }
 }
 
 module.exports = insertionSort;

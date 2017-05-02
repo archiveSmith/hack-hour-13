3902e8ec57a1643028971cdb3029ce260731e96b
Mary-Snow <snow.mary.r@gmail.com>
2017-05-02 09:33:44 -0700

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,34 @@
 
 
 
-function insertionSort(array) {
+// function insertionSort(array) {
+//   let len = array.length; 
+//   for(let i = 0; i < array.length; i ++){
+//     let temp = array[i]
+//     for(let j = i - 1; j >= 0 && (array[j] > temp); j --){
+//       array[j + 1] = array[j]
+//     }
+//     array[j +1] = temp;
+//   }
+// }
 
+function insertionSort(arr) {  
+  //iterate through the array
+    for (var i = 1; i < arr.length; i++) {
+      //copy the current element in the array
+        var tmp = arr[i]; 
+        /*Check through the sorted part and compare with the number in tmp. If large, shift the number*/
+        for (var j = i - 1; j >= 0 && (arr[j] > tmp); j--) {
+            //Shift the number
+            arr[j + 1] = arr[j];
+        }
+        //Insert the copied number at the correct position
+        //in sorted part. 
+        arr[j + 1] = tmp;
+    }
 }
 
+const myArr = [5,3,1,2,4]
+insertionSort(myArr)
+console.log(myArr)
 module.exports = insertionSort;

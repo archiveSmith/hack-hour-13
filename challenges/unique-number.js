a090a5657b7f4b1da1860a3e6e8349efe50b8c2e
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-03 09:24:59 -0700

    completed unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,22 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
 
+//O(n) time
+function uniqueNumber(array) {
+  const cache = {};
+  for (let i = 0; i < array.length; i += 1) {
+    if (cache[array[i]]) delete cache[array[i]];
+    else cache[array[i]] = true;
+  }
+  return Object.keys(cache)[0];
 }
+
+//O(1) space
+// function uniqueNumber(array) {
+//   for (let i = 0; i < array.length - 1; i += 1) {
+//     if (!array.slice(i + 1).includes(array[i])) return array[i];
+//   }
+// }
 
 module.exports = uniqueNumber;

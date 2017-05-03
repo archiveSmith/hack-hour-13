ebeba5cb9c979a49319ee56f7f5de817ee6217d2
mkhouv <mkhouv@gmail.com>
2017-05-03 09:45:27 -0700

    completed hack hours

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
+  for (let i = 0; i < array.length - 1; i++){
+    insert(array, i, array[i + 1]);
+  }
+  var insert = function (array, rightIndex, value) {
+    for (var j = rightIndex; j >= 0 && array[j] > value; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = value;
+  };
 
+  for (var i = 0; i & lt; array.length - 1; i++){
+    insert(array, i, array[i + 1]);
+  }
+};
 }
 
 module.exports = insertionSort;
--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,12 @@
  *
  */
 function uniqueNumber(array) {
+  let output = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    output ^= array[i]; 
+  }
 
+  return output;
 }
 
 module.exports = uniqueNumber;

1129734623a154b91b5bca62d877b8fcb356e955
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-04 10:13:17 -0700

    finished get all product

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,64 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
+// function getAllProducts(array) {
+//   // declare a result where the products will be stored and product to hold each array.length - 1 products
+//   const result = [];
+//   let product;
 
+//   // create a recursive function to iterate through the array and multiply them together
+//   function product(subArr, length) {
+//     // if the product is array.length - 1 long, then return the value
+//     if (length === array.length - 1) {
+//       result.push(product);
+//       // have to divide by the last number
+//       product = 1;
+//       return;
+//     }
+
+//     product *= subArr[arr.length - 1];
+
+//     return product(subArr.slice(0, array.length - 1), length++)
+//   } 
+
+//   product(array, 0);
+
+//   return result;
+// }
+
+// function getAllProducts(array) {
+//   // have a result array and a product variable
+//   const result = [];
+//   let history = [];
+//   let product = 1;
+//   // loop through array starting from the back
+//   for (let i = array.length - 1; i >= 0; i -= 1) {
+//     product *= array[i];
+//     history.push(product);
+//     // j will be the value immediately before i
+//     for (let j = i - 1; j >= 0; j += 1) {
+//       product *= array[j];
+//       history.push(product);
+//       // if it hits a multiple of array.length -1, then resset values to prev
+//       if (history.length === array.length - 1) {
+//         result.push(product);
+//         history.pop();
+//         product = history[history.length - 1];
+//       } 
+//     }
+//   }
+// }
+
+function getAllProducts(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    result.push(array.reduce((acc, curr, index) => {
+      return index !== i ? acc * curr : acc;
+    }, 1))
+  }
+
+  return result;
 }
 
 module.exports = getAllProducts;

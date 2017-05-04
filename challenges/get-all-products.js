4e715a937f34838312a558ef4ade60bbcf166e97
Mary-Snow <snow.mary.r@gmail.com>
2017-05-04 09:35:22 -0700

    recursive solution to get all products

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,17 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
-
+function getAllProducts(array, productArr = []) {
+  if(array.length === productArr.length) return productArr;
+  let product = 1;
+  for(let i = 0; i < array.length -1; i ++){
+    product *= array[i]
+    console.log(array[i])
+  }
+  productArr.push(product);
+  let myArr = array.slice(1, array.length).concat(array.slice(0, 1))
+  return getAllProducts(myArr, productArr);
 }
+getAllProducts([1, 7, 3, 4]); //->  [84, 12, 28, 21]
 
 module.exports = getAllProducts;

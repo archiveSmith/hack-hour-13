ab218ae21bbcb24fb8b1a05f1ff1c4705aab0248
Jordan Bautista <jordanbau@gmail.com>
2017-05-05 09:36:26 -0700

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,44 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  const node = new Node(val);
+
+  if (this.head === null) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  if (this.head.val === val) this.head = this.head.next;
+
+  let curr = this.head;
+  let removed = false;
+
+  while (curr.next) {
+    if (curr.val === val) {
+      curr.next.prev = curr.prev;
+      curr.prev.next = curr.next;
+      curr = this.tail;
+      removed = true;
+    }
+    else curr = curr.next;
+  }
+  if (this.tail.val === val) {
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    removed = true;
+  }
+
+  if (!removed) return 'Value not found in list';
 };
 
 module.exports = LinkedList;

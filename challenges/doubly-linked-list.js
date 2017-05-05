f416018b90cd04d6e8a0f0c418ba3a58e1f4bfec
Jeffrey Ma <jjeffreyma@gmail.com>
2017-05-05 09:31:06 -0700

    Initial approach

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,28 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let currlast = this.tail;
+  currlast.next = new Node(val);
+  this.tail = currlast.next;
+  this.tail.prev = currlast;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  function traverse(node) {
+    if (!node || node.next === null) return;
+    if (node.val === val) {
+      let previous = node.prev;
+      let next = node.next;
+
+      previous.next = next;
+      next.prev = previous;
+    }
+    traverse(node.next);
+  }
+  traverse(this.head);
 };
 
 module.exports = LinkedList;

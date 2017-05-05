b56341b14e3169915588dd4e21ba823b144d1dee
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-05 09:52:02 -0700

    completed doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,29 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  const newNode = new Node(val);
+  if (this.head) {
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  } else {
+    this.head = this.tail = newNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let curr = this.head;
+  while (curr && curr.val !== this.val) curr = curr.next;
+  if (curr) {
+    if (curr.prev) curr.prev.next = curr.next;
+    if (curr.next) curr.next.prev = curr.prev;
+    if (curr === this.head) this.head = curr.next;
+    if (curr === this.tail) this.tail = curr.prev;
+  }
 };
 
 module.exports = LinkedList;

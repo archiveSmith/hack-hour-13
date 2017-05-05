3286215ca758169d18f806131afc6c625f6e84bc
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-05 09:47:45 -0700

    finished dobule linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,44 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  // create new Node that will go at the end of list, then reassign the values
+  let newTail = new Node(val);
+  newTail.prev = this.tail;
+  this.tail.next = newTail;
+  this.tail = newTail;
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  for (let i = this.head; i; i = i.next) {
+    if (i.val === val) {
+      // if the value that is being removed is the first value, only reassign the head
+      if (i === this.head) {
+        this.head = i.next;
+        this.head.prev = null;
+      } else if (i === this.tail) { // if the value is the last value, only reassign the tail
+        this.tail = i.prev;
+        this.tail.next = null;
+      } else { // for everything in between, reassign both the next and the prev value
+        i.prev.next = i.next;
+        i.next.prev = i.prev;
+      }
+      break;
+    }
+  }
 };
+
+/*
+  let head = new LinkedList();
+  head.head = head.tail = new Node(5);
+  head.add(10)
+  head.add(20)
+  console.log('first', head);
+  head.remove(10);
+  console.log('second', head)
+*/
 
 module.exports = LinkedList;

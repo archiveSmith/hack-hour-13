a5fcad3a81d769e6f67a3601ef7da33bddc60958
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-06 09:30:12 -0700

    completed circle-country

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -1,7 +1,8 @@
 /**
  *  Circle Country is a country that contains several circular-shaped districts.
  *  Some districts may be situated inside other districts, but their borders do not intersect or touch.
- *  Tyus is a resident of Circle Country. When he travels between two locations, he always tries to cross the fewest number of district borders as possible.
+ *  Tyus is a resident of Circle Country. When he travels between two locations, he always tries to cross
+ *  the fewest number of district borders as possible.
  *
  *  You are given an array x, an array y, and an array r
  *
@@ -22,8 +23,18 @@
  *
  */
 
-function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
+function getDistance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
 
+function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
+  const crossedCircles = 0;
+  for (let i = 0; i < x.length; i += 1) {
+    const startsInside = getDistance(x[i], y[i], start_x, start_y) < r[i];
+    const endsInside = getDistance(x[i], y[i], end_x, end_y) < r[i];
+    if (startsInside ^ endsInside) crossedCircles += 1;
+  }
+  return crossedCircles;
 }
 
 module.exports = circleCountry;

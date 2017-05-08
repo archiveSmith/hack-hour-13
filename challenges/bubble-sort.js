0b31d526ddf0ca53057d8d3b324aab737bb11980
Jeffrey Ma <jjeffreyma@gmail.com>
2017-05-08 09:26:25 -0700

    Initial approach

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,17 @@
 
 
 function bubbleSort(array) {
-
+  let notSorted = true;
+  while (notSorted) {
+    notSorted = false;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        notSorted = true;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;

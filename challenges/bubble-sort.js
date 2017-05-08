5f7530e3f2ef2d09022568e5763b86753aff92ea
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-08 09:37:58 -0700

    completed bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,12 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;

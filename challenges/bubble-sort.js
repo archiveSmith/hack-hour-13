5d9b001c73e3a995a8ed4e63f7ad79909c1de8d0
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-05-08 09:46:35 -0700

    buble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+let swapped = true;
+	while (swapped === true) {
+		swapped = false;
+		for (let curr = 0; curr < array.length - 1; curr++) {
+			let next = curr + 1;
+			if (array[curr] > array[next]) {
+				swapped = true;
+				[array[curr],array[next]] = [array[next], array[curr]];
+			}
+		}
+	}
+	return array;
 }
 
 module.exports = bubbleSort;

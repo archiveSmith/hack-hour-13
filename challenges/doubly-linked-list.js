921dcc4b3696ff9906978397a2cdbe146804f18f
Darrick Mckirnan <dtmckirnan@Darricks-MacBook-Pro.local>
2017-05-08 09:43:56 -0700

    first add of doubly linked

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,25 +16,33 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  let newNode = Node(val);
-  let curr;
+LinkedList.prototype.add = function (val) {
+  let newNode = new Node(val);
 
   if (!this.head) {
     this.head = newNode;
     this.tail = newNode;
-    return;
   }
-
-  while (curr) {
-    
+  else if (this.head === this.tail) {
+    this.tail = newNode;
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  } else {
+    this.tail.next = newNode;
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
   }
 };
+
+// const linked = new LinkedList();
+// linked.add(5);
+// linked.add(4);
+// console.log(linked.head);
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
+LinkedList.prototype.remove = function (val) {
   
 };
 

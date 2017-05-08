d6a39ad8718f4f61f29e32a46a5493f4d1e4fe28
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-05-08 11:45:19 -0700

    completed bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+  let unsorted;
+  if (array.length === 1) return array;
+  for (let i = 1; i < array.length; i += 1) {
+    if (i === 1) unsorted = false
+    if (array[i] < array[i-1]) {
+      [array[i], array[i-1]] = [array[i-1], array[i]]
+      unsorted = true;
+    }
+    // console.log(array, i);
+    if (i === array.length-1 && unsorted === true) i = 0;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;

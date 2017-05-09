fe4df5f6c5172a07efd0f26c2755d3f1d46e8fd8
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-09 09:11:29 -0700

    circle

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -24,33 +24,38 @@
 
 //pythagorean theorem a^2 + b^2 = c^2 to find longest edge of a right triangle
 function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
-  let circlesToCross = 0;
+  var circlesToCross = 0;
 
-  for (let i = 0; i < x.length; i += 1) {
-      let distanceStart = 0;
+  for (var i = 0; i < x.length; i++) {
+    
+    var distanceStart = Math.sqrt((x[i] - start_x)
+      * (x[i] - start_x) + (y[i] - start_y) * (y[i] - start_y));
+
+    var distanceEnd = Math.sqrt((x[i] - end_x)
+      * (x[i] - end_x) + (y[i] - end_y) * (y[i] - end_y));
+
+    var radius = r[i];
+    //if he starts inside a circle, or he needs to go inside a circle to get to end point, then increment
+    if (distanceStart < radius && distanceEnd > radius ||
+      distanceStart > radius && distanceEnd < radius) {
+      circlesToCross++;
+    }
   }
+  return circlesToCross;
 }
 
 //////////////////////////////////////////////
-let x = [1,-2,5];  //x[i] is center of each circle
-let y = [2,-4,-3];  //y[i] is center of each circle
-let r = [1,3,2];  //r[i] is radius
+let x = [1, -2, 5];  //x[i] is center of each circle
+let y = [2, -4, -3];  //y[i] is center of each circle
+let r = [1, 3, 2];  //r[i] is radius
 let start_x = -3;  //starting coordinates
 let start_y = 2;
 let end_x = 5;  //ending coordinates
 let end_y = 2;
-console.log( circleCountry(x, y, r, start_x, start_y, end_x, end_y) )
+console.log(circleCountry(x, y, r, start_x, start_y, end_x, end_y))
 
 
-/*
-RETURN: (num)  min # of districts must cross 
 
-for (var i = 0; i < steps; i++) {
-    xValues[i] = (centerX + radius * Math.cos(2 * Math.PI * i / steps));
-    yValues[i] = (centerY + radius * Math.sin(2 * Math.PI * i / steps));
-}
-
-*/
 
 
 module.exports = circleCountry;

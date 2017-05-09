6cd7a765b894806e596fe720435a43b69d4150fd
Mary-Snow <snow.mary.r@gmail.com>
2017-05-09 09:58:09 -0700

    solutionfor bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,15 +4,39 @@
 
 
 
+// function bubbleSort(array) {
+
+//   for(let i = 0; i < array.length; i++){
+//     for(let j = i +1; j < array.length; j ++){
+//       if(array[i] > array[j]) [array[i], array[j]] = [array[j], array[i]]
+//     }
+//   }
+//  return array;
+// }
+
+// SOLUTION
 function bubbleSort(array) {
-  
-  for(let i = 0; i < array.length; i++){
-    for(let j = i +1; j < array.length; j ++){
-      if(array[i] > array[j]) [array[i], array[j]] = [array[j], array[i]]
-    }
+  function swap(arr, ind1, ind2) {
+    [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]];
   }
- return array;
+  const newArr = array;
+  let i = array.length - 1;
+  let changed = true;
+  while (i > 0 && changed) {
+    changed = false;
+    let j = 0;
+    while (j < i) {
+      if (newArr[j] > newArr[j + 1]) {
+        swap(newArr, j, j + 1)
+        changed = true;
+      }
+      j += 1;
+    }
+    i -= 1;
+  }
+  return newArr;
 }
 
-console.log(bubbleSort([4,5,61,324,7,1,123,0]))
+
+console.log(bubbleSort([4, 5, 61, 324, 7, 1, 123, 0]))
 module.exports = bubbleSort;

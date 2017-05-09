42b305ece247817808ad195fde48caa074ef9338
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-05-09 11:28:59 -0700

    phonebook

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let shouldLoop = true;
+  while (shouldLoop) {
+    shouldLoop = false;
+    let i = array.length - 1;
+    for (let j = 0; j < i; j += 1) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        shouldLoop = true;
+      }
+    }
+    i -= 1;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;

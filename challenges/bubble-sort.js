e120ebd6763ba2eba5f3e0352d4a58bfc33dcf9f
Mary-Snow <snow.mary.r@gmail.com>
2017-05-09 09:14:43 -0700

    minor changes to bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,6 +5,7 @@
 
 
 function bubbleSort(array) {
+  
   for(let i = 0; i < array.length; i++){
     for(let j = i +1; j < array.length; j ++){
       if(array[i] > array[j]) [array[i], array[j]] = [array[j], array[i]]

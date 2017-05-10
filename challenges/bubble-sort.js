3ba5a0a1b6df94f10d192587a59f28b4aa17df88
slhstudio <slhstudio@gmail.com>
2017-05-10 13:19:10 -0700

    done count stairs

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -19,3 +19,27 @@ function bubbleSort(array) {
 }
 
 module.exports = bubbleSort;
+
+//john's solution:
+// function bubbleSort(array) {
+//   function swap(arr, ind1, ind2) {
+//     [arr[ind1], arr[ind2]] = [arr[ind2], arr[ind1]];
+//   }
+//   const newArr = Array.from(array);
+//   let i = array.length - 1;
+//   let changed = true;
+
+//   while (i > 0 && changed) {
+//     changed = false;
+//     let j = 0;
+//     while (j < i) {
+//       if (newArr[j] > newArr[j + 1]) {
+//         swap(newArr, j, j + 1);
+//         changed = true;
+//       }
+//       j += 1;
+//     }
+//     i -= 1;
+//   }
+//   return newArr;
+// }

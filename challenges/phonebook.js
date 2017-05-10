8d1f76203c0c23b64241b73896f7358471e39cb0
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-10 09:14:18 -0700

    phonebook

---
+++ challenges/phonebook.js
@@ -0,0 +1,116 @@
+/**
+* Part 1:
+* You are given an array of arrays.  The subarrays are first names and phone numbers.
+*
+* Example:
+*   jazbook = [
+*     ['alex','301-844-3421'],
+*     ['jae','301-844-1211'],
+*     ['david','301-844-0978'],
+*     ['travis','301-844-8505']
+*     ['jasmine','1800-974-4539'],
+*   ];
+*
+* jazbooks are not always sorted...
+*
+* Develop a function that takes in a jazbook and a name, searches through the jazbook and
+* returns the persons phone number. If the person does not exists, return false.
+*
+* How efficient can you make this?
+* Part 2: 
+* Why are we storing names and phone numbers in an array?
+* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
+* complete with methods to add new names and look up and remove existing entries
+*/
+
+function findName(jazbook, name) {
+  // check that jazbook is an array
+  if (jazbook.constructor === Array && typeof name === 'string') {
+    for (let i = 0; i < jazbook.length; i += 1) {
+      // makes sure that each name and phone number is an array
+      if (jazbook[i].constructor !== Array) break;
+      if (jazbook[i][0] === name) return jazbook[i][1];
+    }
+  }
+  return false;
+}
+
+function makePhoneBookObject(jazbook) {
+  this.phonebook = jazbook.reduce((acc, curr) => {
+    acc[curr[0]] = curr[1];
+    return acc;
+  }, {})
+}
+
+makePhoneBookObject.prototype.add = (name, phonenumber) => {
+  this.phonebook[name] = phonenumber;
+}
+
+makePhoneBookObject.prototype.remove = (name) => {
+  delete this.phonebook[name];
+}
+
+const jazbook = [
+  ['alex', '301-844-3421'],
+  ['jae', '301-844-1211'],
+  ['david', '301-844-0978'],
+  ['travis', '301-844-8505'],
+  ['jasmine', '1800-974-4539']
+];
+
+console.log(findName(jazbook, 'travis'))
+
+module.exports = {
+  findName,
+  makePhoneBookObject,
+};
+
+/**
+* Part 1:
+* You are given an array of arrays.  The subarrays are first names and phone numbers.
+*
+* Example:
+*   jazbook = [
+*     ['alex','301-844-3421'],
+*     ['jae','301-844-1211'],
+*     ['david','301-844-0978'],
+*     ['travis','301-844-8505']
+*     ['jasmine','1800-974-4539'],
+*   ];
+*
+* jazbooks are not always sorted...
+*
+* Develop a function that takes in a jazbook and a name, searches through the jazbook and
+* returns the persons phone number. If the person does not exists, return false.
+*
+* How efficient can you make this?
+* Part 2: 
+* Why are we storing names and phone numbers in an array?
+* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
+* complete with methods to add new names and look up and remove existing entries
+*/
+
+//  return the number associated with the name in the jazbook
+// function findName(jazbook, name) {
+//   if(!Array.isArray(jazbook) || !jazbook.length || typeof name !== 'string') return false;
+
+//   for (let i = 0; i < jazbook.length; i += 1) {
+//     if (jazbook[i][0] === name) return jazbook[i][1];
+//   }
+//   return false;
+// }
+
+// // return an object literal representing the jazbook
+// function makePhoneBookObject(jazbook) {
+//   return jazbook.reduce((jazbookObj, subarray) => {
+//     jazbookObj[subarray[0]] = subarray[1];
+//     return jazbookObj;
+//   }, {});
+// }
+
+// const objectToExport = {
+//   findName,
+//   makePhoneBookObject,
+// };
+
+// module.exports = objectToExport;

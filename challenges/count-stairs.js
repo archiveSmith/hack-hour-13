0719539f91488beb4ad5e9a54b5b306a350a8071
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-11 10:12:00 -0700

    finished new-intersections

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,29 +14,44 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
+// function countStairs(n) {
+//   if (!Number.isInteger(n)) {
+//     return false;
+//   }
+
+//   let count = 0;
+
+//   function stairs(n, decrement) {
+//     if (n <= 0) {
+//       if (n === 0) {
+//         count += 1;
+//       }
+//       return
+//     }
+//     const otherNum = decrement === 1 ? 2 : 1;
+//     stairs(n - decrement, decrement) || stairs(n - otherNum, otherNum);
+//   }
+
+//   stairs(n, 1);
+
+//   return count;
+// }
+
+// my solution
+// function countStairs(n, decrement = 1) {
+//   if (!Number.isInteger(n)) return false;
+  
+//   const otherNum = decrement === 1 ? 2 : 1;
+  
+//   return n === 0 ? 1 : n < 0 ? 0 : countStairs(n - decrement, decrement) + countStairs(n - otherNum, otherNum);
+// }
+
 function countStairs(n) {
-  if (!Number.isInteger(n)) {
-    return false;
-  }
-  
-  let count = 0;
-
-  function stairs(n, decrement) {
-    if (n <= 0) {
-      if (n === 0) {
-        count += 1;
-      }
-      return
-    }
-    const otherNum = decrement === 1 ? 2 : 1;
-    stairs(n - decrement, decrement) || stairs(n - otherNum, otherNum);
-  }
-
-  stairs(n, 1);
-  
-  return count;
+  if (n === 0) return 1;
+  if (n < 0) return 0;
+  return countStairs(n - 1) + countStairs(n - 2);
 }
 
-// countStairs(5);
+console.log(countStairs(5));
 
 module.exports = countStairs
---
+++ challenges/new-intersections.js
@@ -0,0 +1,69 @@
+/**
+ *
+ * You are given an array x and an array y that represent the coordinates of several OLD points
+ * 
+ *   - x is the array of x-coordinates and y is the array of y-coordinates 
+ *   - (x[i], y[i]) correspond to coordinates of the i'th OLD point
+ *
+ * Write an algorithm to find the number of NEW points that can be placed 
+ * such that there are OLD points above, below, to the left, and to the right of the NEW point
+ *
+ *   - 'OLD directly above NEW' means the NEW x-coordinate = OLD x-coordinate & NEW y-coordinate < OLD y-coordinate
+ *
+ * Constraints and Notes:
+ *
+ * 	 - x and y will contain the same number of elements
+ *   - if a new point is bounded by old points and lands on an old point, then count it
+ * 	 
+ */
+
+function newIntersections(x, y) {
+  // check to see if arrays are valid 
+  if (x.constructor !== Array || y.constructor !== Array || x.length !== y.length) {
+    return false;
+  }  
+  
+  // if there is less than 4 points in the array return 0 
+  if (x.length < 4) return 0;
+
+  // create a counter that keeps track of how many points found
+  let totalNewPoints = 0;
+
+  // create a new array with x and y co-ordinates for easier reading
+  const coords = x.map((xIndex, index) => {
+    return { 'x': xIndex, 'y': y[index] }
+  })
+
+  // find all of the x's that are the same value
+  for (let i = 0; i < coords.length; i += 1) {
+    const currCoord = coords[i];
+    for (let j = i; j < coords.length; j += 1) {
+      let doBothExist = 0;
+      // find the next x that has the same value
+      if (coords[j].x === currCoord.x) {
+        // check to see if the larger y minus the smaller y is distance of 2 away from each other (abs val)
+        if (Math.abs(coords[j].y - currCoord.y === 2)) {
+          // check to see if compliments of x and y exist in array
+          for (let k = 0; k < coords.length; k += 1) {
+            if (coords[k].x === coords[j].y || coords[k].x === currCoord.y) {
+              if (coords[k].y === coords[j].x || coords[k].y === currCoord.x) {
+                doBothExist += 1;
+              }
+            }
+          }
+        }
+      }
+      if (doBothExist === 2) {
+        totalNewPoints += 1;
+      }
+      doBothExist = 0;
+    }
+  }
+  // check to see if the compliments of x and y exist
+  // if they do, add one to the counter
+  return totalNewPoints;
+}
+
+// newIntersections([1,2,2,3,4,3], [2,1,3,2,3,4]) ===> 2
+
+module.exports = newIntersections;

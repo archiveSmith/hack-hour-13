000a695167b04d95e8a81f1c1a918da6a383dd6c
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-05-12 09:44:55 -0700

    completed knight-jumps

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -9,9 +9,37 @@
 // from position x = 4 and y = 5.
 //  example input:
 // var str = "(4 5)"
+// x - 1 y - 3
+// x - 1 y + 3
+// x + 1 y + 3
+// x + 1 y - 3
+// x - 3 x - 1
+// x - 3 x + 1
+// x + 3 y - 1
+// x + 3 y + 1
+
+const canMove = (x, y) => {
+  if (x <= 8 && x >= 1 && y <= 8 && y >= 1) return true
+  else return false
+}
 
 function knightjumps(str) {
-
-}
+  let possibilities = 0;
+  const arr = str.slice(1, str.length - 1).split(' ');
+  const x = Number(arr[0]);
+  const y = Number(arr[1]);
+  console.log(arr);
+  if (canMove(x - 1, y - 3)) possibilities += 1;
+  if (canMove(x - 1, y + 3)) possibilities += 1;
+  if (canMove(x + 1, y + 3)) possibilities += 1;
+  if (canMove(x + 1, y - 3)) possibilities += 1;
+  if (canMove(x - 3, x - 1)) possibilities += 1;
+  if (canMove(x - 3, x + 1)) possibilities += 1;
+  if (canMove(x + 3, y - 1)) possibilities += 1;
+  if (canMove(x + 3, y + 1)) possibilities += 1;
+  return possibilities;
+};
+// var str = "(6 6)"
+// console.log(knightjumps(str));
 
 module.exports = knightjumps;

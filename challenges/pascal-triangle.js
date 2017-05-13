23b20fe70900db41b5f12c95b2f14b4a700840d1
Glenn Alexander <glennalexander2014@u.northwestern.edu>
2017-05-13 09:37:44 -0700

    completed pascal-triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,20 @@
 */
 
 function pascalTriangle(numRows) {
-
+  if (numRows === 0) return [];
+  if (numRows === 1) return [[1]];
+  const output = [[1]];
+  for (let i = 2; i <= numRows; i += 1) {
+    const newRow = [];
+    const lastRow = output[output.length - 1];
+    newRow.push(1);
+    for (let j = 0; j < lastRow.length - 1; j += 1) {
+      newRow.push(lastRow[j] + lastRow[j + 1]);
+    }
+    newRow.push(1);
+    output.push(newRow);
+  }
+  return output;
 }
 
 module.exports = pascalTriangle;

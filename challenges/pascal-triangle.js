f9ab832e26edc51cf62dea55aeccc4b2dedab93b
Will Rigsbee <willrigsbee@Users-MacBook-Pro.local>
2017-05-13 09:39:33 -0700

    completed pascal-triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -26,14 +26,26 @@
     [1],
     [1,1],
     [1,2,1],
-    [1,3,3,1],
+    [1,3,3,1],  0, 1 = 1 / 1, 2 = 2 / 2,3
     [1,4,6,4,1],
     [1,5,10,10,5,1]
   ]
 */
 
 function pascalTriangle(numRows) {
-
+  if (numRows === 0) return [[]];
+  const result = [[1]];
+  for (let i = 1; i < numRows; i += 1) {
+    result.push([]);
+    for (let j = 0; j < i; j += 1) {
+      result[i].push(
+        Number(result[i-1][j - 1] === undefined ? 0 : result[i-1][j - 1])
+        + Number(result[i-1][j] === undefined ? 0 : result[i-1][j])
+      );
+    }
+    result[i].push(1);
+  }
+  return result;
 }
 
 module.exports = pascalTriangle;

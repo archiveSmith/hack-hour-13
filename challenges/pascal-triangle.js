b6cf01b8a0bbd9167474030c19c196dbba03ff7a
John C <John@Johns-MacBook-Air.local>
2017-05-16 09:10:03 -0700

    nailed it

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,25 @@
 */
 
 function pascalTriangle(numRows) {
+  if (numRows === 1) return [[1]];
+  if (numRows === 2) return [[1], [1, 1]];
 
+  // add the row above by one less than the current element and at the same position
+  // as the current element
+  let rows = numRows - 2;
+  const pascTri = [[1], [1, 1]];
+  while (rows > 0) {
+    // iterate through the most recent row
+    const rowAbove = pascTri[pascTri.length - 1];
+    const newRow = [1];
+    for (let i = 1; i < rowAbove.length; i += 1) {
+      newRow.push(rowAbove[i - 1] + rowAbove[i]);
+    }
+    newRow.push(1);
+    pascTri.push(newRow);
+    rows -= 1;
+  }
+  return pascTri;
 }
 
 module.exports = pascalTriangle;

164f24661e1e575d64dd218c5d0a26e86b11edad
slhstudio <slhstudio@gmail.com>
2017-08-01 20:29:23 -0700

    fixed

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -8,13 +8,13 @@ findInOrderedSet(nums, 2);  -> false
  */
 
  //O(log n);
- //does not work for neg nunbers!!
+
 function findInOrderedSet(arr, target) {
   function searchHalf(start, end) {
     
     let mid = Math.floor((start + end) / 2); //can't use array.length here b/c mid must adjust either down or up as we look at smaller pieces of array.
     if (arr[mid] === target) return true;
-    if (end === start) return false; 
+    if (start + 1 > end) return false; 
     
     if (target < arr[mid]) {
       return searchHalf(start, mid - 1);
@@ -22,10 +22,50 @@ function findInOrderedSet(arr, target) {
         return searchHalf(mid + 1, end);
     }
   }
-  return searchHalf(-1, arr.length); 
+  return searchHalf(0, arr.length - 1); 
 }
 
 module.exports = findInOrderedSet;
+
+//iterative:
+// function binarySearch(target, nums) {
+//     // see if target appears in nums
+
+//     var start = -1;
+//     var end = nums.length;
+    
+
+//     // if there isn't at least 1 index between start and end,
+//     // we've run out of guesses and the number must not be present
+//     while (start + 1 < end) {
+
+//         // find the index ~halfway between the start and end
+//         // we have to round down, to avoid getting a "half index"
+       
+//         var mid = Math.floor((start + end) / 2);
+//         console.log(mid);
+
+//         if (nums[mid] === target) {
+//             return true;
+//         }
+
+//         if (nums[mid] > target) {
+
+//             // target is to the left, so move end to the left
+//              end = mid;
+
+//         } else {
+
+//             // target is to the right, so move start to the right
+//            start  = mid;
+//         }
+//     }
+
+//     return false;
+// }
+
+
+
 
 // // return arr.indexOf(target) !== -1 // O(n)
 

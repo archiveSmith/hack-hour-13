ea5b28d5a0b54ed654bbd7139927814b6ad9f4ce
slhstudio <slhstudio@gmail.com>
2018-02-23 17:13:54 -0800

    test

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,28 +12,43 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
-  if (!string) return [''];
-  const array = string.split('');
+// function anagrams(string) {
+//   if (!string) return [''];
+//   const array = string.split('');
   
-  function again (letters) {
-    let results = [];
-    if (letters.length <= 1) {
-      return results.concat(letters);
+//   function again (letters) {
+//     let results = [];
+//     if (letters.length <= 1) {
+//       return results.concat(letters);
+//     }
+//     for (let i = 0; i < letters.length; i++) {
+//       const front = letters[i];
+//       const theRest = letters.slice(0, i).concat(letters.slice(i + 1));
+//       results = results.concat(again(theRest).map(str => front + str));
+//     }
+//     return results;
+//   }
+//   // //last resort to get rid of duplicates: should be done somehow above
+//   // return final = again(array).filter((item, index) => {
+//   //   return again(array).indexOf(item) === index;
+//   // })
+//   return again(array);
+// }
+function anagrams (word) {
+    if (word.length < 2) {
+      return [word];
+    } else {
+      var allAnswers = [];
+      for (var i = 0; i < word.length; i++) {
+        var letter = word[i];
+        var shorterWord = word.substr(0, i) + word.substr(i + 1, word.length - 1);
+        var shortwordArray = allAnagrams(shorterWord);
+        for (var j = 0; j < shortwordArray.length; j++) {
+          allAnswers.push(letter + shortwordArray[j]);
+        }
+      }
+      return allAnswers;
     }
-    for (let i = 0; i < letters.length; i++) {
-      const front = letters[i];
-      const theRest = letters.slice(0, i).concat(letters.slice(i + 1));
-      results = results.concat(again(theRest).map(str => front + str));
-    }
-    return results;
-  }
-  // //last resort to get rid of duplicates: should be done somehow above
-  // return final = again(array).filter((item, index) => {
-  //   return again(array).indexOf(item) === index;
-  // })
-  return again(array);
 }
-
 module.exports = anagrams;
 //console.log(anagrams('cats'));

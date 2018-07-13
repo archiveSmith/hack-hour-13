c8bb81755e0f45c3d50858a42d79d730ce0a5cc5
slhstudio <slhstudio@gmail.com>
2018-07-13 12:59:05 -0700

    solution

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -9,31 +9,51 @@
   // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
+function commonElements(array1, array2, array3, array4){
+
+  var obj = {};
+  arguments[0].forEach(function(item, index) {
+    obj[item] = 1;
+  });
+
+  for (var i = 1; i < arguments.length; i++) {
+    arguments[i].forEach(function(item) {
+      if (obj[item] === i) obj[item] = i + 1;
+    });
+  }
+
+  for (var key in obj) {
+    if (obj[key] < 4) delete obj[key];
+   }
+
+   if (Object.keys(obj).length === 0) return "Nothing in Common!"
+   return Object.keys(obj);
+ }
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
-  const objAll = {};
-  const arrays = [...arguments];
-  //this loop, basically linear through all arrays, makes obj of all elements, not counting dups
-  for (let i = 0; i < 4;i++) {
-    const obj = {};
-    for (let j = 0; j < arrays[i].length; j++) {
-       obj[arrays[i][j]] ? obj[arrays[i][j]] : obj[arrays[i][j]] = 1;
-    }
-    //this loop makes master obj of all elements, counting how many objs each appears in
-    const keys = Object.keys(obj);
-    keys.forEach(key => {
-      objAll[key] ? objAll[key]++ : objAll[key] = 1;
-    })
-  }
-  //this makes array of any elements appearing 4 times, ie in each array.
-  const result = [];
-  for (const k in objAll) {
-    if (objAll[k] === 4) result.push(k);
-  }
-  if (!result.length) return 'Nothing in Common!'
-  return result;
-}
+// function commonElements(array1, array2, array3, array4){
+//   const objAll = {};
+//   const arrays = [...arguments];
+//   //this loop, basically linear through all arrays, makes obj of all elements, not counting dups
+//   for (let i = 0; i < 4;i++) {
+//     const obj = {};
+//     for (let j = 0; j < arrays[i].length; j++) {
+//        obj[arrays[i][j]] ? obj[arrays[i][j]] : obj[arrays[i][j]] = 1;
+//     }
+//     //this loop makes master obj of all elements, counting how many objs each appears in
+//     const keys = Object.keys(obj);
+//     keys.forEach(key => {
+//       objAll[key] ? objAll[key]++ : objAll[key] = 1;
+//     })
+//   }
+//   //this makes array of any elements appearing 4 times, ie in each array.
+//   const result = [];
+//   for (const k in objAll) {
+//     if (objAll[k] === 4) result.push(k);
+//   }
+//   if (!result.length) return 'Nothing in Common!'
+//   return result;
+// }
 
 module.exports = commonElements;
